5b9556b7b6aa83acb322666e2354d7c1016cc052
Marius Glittum <Marius.Glittum@nav.no>
2019-05-28 13:52:14 +0200

    Oppdatert vtp properties

--- domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/abakus/vedtak/kafka/ApplicationIdUtil.java
+++ domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/abakus/vedtak/kafka/ApplicationIdUtil.java
@@ -1,24 +1,11 @@
 package no.nav.foreldrepenger.abakus.vedtak.kafka;
 
-import java.net.Inet4Address;
-import java.net.UnknownHostException;
-import java.util.UUID;
-
 public final class ApplicationIdUtil {
 
     private ApplicationIdUtil() {
     }
 
     public static String get() {
-        String hostname;
-        try {
-            hostname = System.getenv("hostname"); // settes på pod i cluster (pod identifikator)
-            if (hostname == null) {
-                hostname = Inet4Address.getLocalHost().getHostName() + "-" + UUID.randomUUID();
-            }
-        } catch (NullPointerException | SecurityException | UnknownHostException e) {
-            hostname = System.getProperty("nais.app.name", "java-application") + "-" + UUID.randomUUID();
-        }
-        return hostname;
+        return System.getProperty("nais.app.name", "fpabakus-");
     }
 }
