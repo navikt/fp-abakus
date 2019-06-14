cefffb9c503585ba16633a8f9a75369773063184
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-06-14 14:12:19 +0200

    Update V1.1_23__manglende_kodeverk.sql

--- migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_23__manglende_kodeverk.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_23__manglende_kodeverk.sql
@@ -1,3 +1,35 @@
+INSERT INTO KODEVERK (kode, navn, beskrivelse, kodeverk_eier)
+VALUES
+  ('ARBEIDSFORHOLD_HANDLING_TYPE',
+   'Kodeverk over gyldige typer av handlinger saksbehandler kan utføre av overstyringer på arbeidsforhold', '',
+   'VL');
+INSERT INTO KODELISTE (id, kode, navn, beskrivelse, gyldig_fom, kodeverk)
+VALUES (nextval('seq_kodeliste'), '-', 'Udefinert', 'Udefinert', to_date('2000-01-01', 'YYYY-MM-DD'),
+        'ARBEIDSFORHOLD_HANDLING_TYPE');
+INSERT INTO KODELISTE (id, kode, navn, beskrivelse, gyldig_fom, kodeverk)
+VALUES
+  (nextval('seq_kodeliste'), 'BRUK', 'Bruk', 'Bruk', to_date('2000-01-01', 'YYYY-MM-DD'), 'ARBEIDSFORHOLD_HANDLING_TYPE');
+INSERT INTO KODELISTE (id, kode, navn, beskrivelse, gyldig_fom, kodeverk)
+VALUES (nextval('seq_kodeliste'), 'BRUK_UTEN_INNTEKTSMELDING', 'Bruk, men ikke benytt inntektsmelding',
+        'Bruk men ikke benytt inntektsmelding', to_date('2000-01-01', 'YYYY-MM-DD'), 'ARBEIDSFORHOLD_HANDLING_TYPE');
+INSERT INTO KODELISTE (id, kode, navn, beskrivelse, gyldig_fom, kodeverk)
+VALUES (nextval('seq_kodeliste'), 'IKKE_BRUK', 'Ikke bruk', 'Ikke bruk', to_date('2000-01-01', 'YYYY-MM-DD'),
+        'ARBEIDSFORHOLD_HANDLING_TYPE');
+INSERT INTO KODELISTE (id, kode, navn, beskrivelse, gyldig_fom, kodeverk)
+VALUES (nextval('seq_kodeliste'), 'SLÅTT_SAMMEN_MED_ANNET', 'Arbeidsforholdet er slått sammen med et annet', 'Arbeidsforholdet er slått sammen med et annet', to_date('2000-01-01', 'YYYY-MM-DD'),
+        'ARBEIDSFORHOLD_HANDLING_TYPE');
+        
+INSERT INTO KODELISTE (id, kode, navn, beskrivelse, gyldig_fom, kodeverk)
+VALUES (nextval('seq_kodeliste'), 'NYTT_ARBEIDSFORHOLD', 'Arbeidsforholdet er ansett som nytt', 'Arbeidsforholdet er ansett som nytt', to_date('2000-01-01', 'YYYY-MM-DD'),
+        'ARBEIDSFORHOLD_HANDLING_TYPE');
+
+
+INSERT INTO KODELISTE (id, kode, beskrivelse, gyldig_fom, kodeverk)
+VALUES (nextval('seq_kodeliste'), 'BRUK_MED_OVERSTYRT_PERIODE', 'Bruk arbeidsforholdet med overstyrt periode', to_date('2000-01-01', 'YYYY-MM-DD'), 'ARBEIDSFORHOLD_HANDLING_TYPE');
+
+INSERT INTO KODELISTE_NAVN_I18N (id, kl_kodeverk, kl_kode, sprak, navn)
+VALUES (nextval('SEQ_KODELISTE_NAVN_I18N'), 'ARBEIDSFORHOLD_HANDLING_TYPE',    'BRUK_MED_OVERSTYRT_PERIODE',   'NB',   'Bruk arbeidsforholdet med overstyrt periode');
+
 INSERT INTO KODELISTE (id, kode, beskrivelse, gyldig_fom, kodeverk)
 VALUES (nextval('seq_kodeliste'), 'LAGT_TIL_AV_SAKSBEHANDLER', 'Arbeidsforhold lagt til av saksbehandler', to_date('2000-01-01', 'YYYY-MM-DD'), 'ARBEIDSFORHOLD_HANDLING_TYPE');
 
