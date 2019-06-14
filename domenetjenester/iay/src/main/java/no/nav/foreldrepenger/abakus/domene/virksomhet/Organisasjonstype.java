f4c4c6282226494dab338accb3dcf18500df6b53
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-06-14 14:21:57 +0200

    FIX manglende koder i ArbeidHendelsetype og Organisasjonstypeæ (#19)
    * FIX manglende koder i ArbeidHendelsetype og Organisasjonstypeæ

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/virksomhet/Organisasjonstype.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/virksomhet/Organisasjonstype.java
@@ -13,9 +13,14 @@ public class Organisasjonstype extends Kodeliste {
 
     public static final Organisasjonstype JURIDISK_ENHET = new Organisasjonstype("JURIDISK_ENHET"); //$NON-NLS-1$
     public static final Organisasjonstype VIRKSOMHET = new Organisasjonstype("VIRKSOMHET"); //$NON-NLS-1$
+    public static final Organisasjonstype KUNSTIG = new Organisasjonstype("KUNSTIG"); //$NON-NLS-1$
 
     public static final Organisasjonstype UDEFINERT = new Organisasjonstype("-"); //$NON-NLS-1$
 
+    /** Orgnr for KUNSTIG organisasjoner. Går sammen med Organisasjonstype#KUNSTIG. (p.t. kun en kunstig organisasjon som holder på arbeidsforhold lagt til av saksbehandler.)*/
+    public static final String KUNSTIG_ORG = "342352362";
+
+    
     @SuppressWarnings("unused")
     private Organisasjonstype() {
         // Hibernate
@@ -23,5 +28,9 @@ public class Organisasjonstype extends Kodeliste {
 
     public Organisasjonstype(String kode) {
         super(kode, DISCRIMINATOR);
+    }
+    
+    public static boolean erKunstig(String orgNr) {
+        return KUNSTIG_ORG.equals(orgNr);
     }
 }
