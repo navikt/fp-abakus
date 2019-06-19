9bd6a1f24ee5fdb1a3916e4dc60c6060fdc84504
Marius Glittum <Marius.Glittum@nav.no>
2019-06-19 08:04:56 +0200

    Legger til tasktype for lagre vedtak task

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_28__lagre-vedtak-task.sql
@@ -0,0 +1,3 @@
+insert into PROSESS_TASK_TYPE (kode, navn, feil_maks_forsoek, feilhandtering_algoritme, beskrivelse)
+values ('vedtakEvent.lagre', 'Lagre vedtatt ytelse even', 1, 'DEFAULT',
+        'Lagrer vedtak mottatt på kafka');
