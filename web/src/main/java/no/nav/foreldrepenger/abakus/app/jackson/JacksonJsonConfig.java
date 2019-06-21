1e633ff9c616b8bb7f15e30d9a0d001642836416
Marius Glittum <Marius.Glittum@nav.no>
2019-06-21 12:03:38 +0200

    Legger til støtte for InjectableValues

--- web/src/main/java/no/nav/foreldrepenger/abakus/app/jackson/JacksonJsonConfig.java
+++ web/src/main/java/no/nav/foreldrepenger/abakus/app/jackson/JacksonJsonConfig.java
@@ -9,6 +9,7 @@ import javax.ws.rs.ext.Provider;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -17,6 +18,7 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import no.nav.foreldrepenger.abakus.app.IndexClasses;
+import no.nav.foreldrepenger.kontrakter.iaygrunnlag.kodeverk.KodeValidator;
 
 @Provider
 public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
@@ -34,6 +36,9 @@ public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
         // KodelisteSerializer og KodeverkSerializer bør i tilfelle også støtte JsonInclude.Include.*
         // objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         objectMapper.registerModule(SER_DESER);
+        InjectableValues.Std std = new InjectableValues.Std();
+        std.addValue(KodeValidator.class, KodeValidator.HAPPY_VALIDATOR);
+        objectMapper.setInjectableValues(std);
 
         objectMapper.registerSubtypes(getJsonTypeNameClasses());
     }
