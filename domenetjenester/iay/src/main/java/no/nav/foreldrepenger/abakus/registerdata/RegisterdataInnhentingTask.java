706e0da92d991b0aca990094ecc85c8a138971b6
Marius Glittum <marius.glittum@nav.no>
2019-10-10 13:53:01 +0200

    Retter feil ved lasting av rett type innhenter ved async innhenting

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/RegisterdataInnhentingTask.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/RegisterdataInnhentingTask.java
@@ -1,8 +1,5 @@
 package no.nav.foreldrepenger.abakus.registerdata;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
@@ -15,7 +12,6 @@ import no.nav.foreldrepenger.abakus.kobling.KoblingTask;
 import no.nav.foreldrepenger.abakus.kobling.KoblingTjeneste;
 import no.nav.foreldrepenger.abakus.kobling.kontroll.YtelseTypeRef;
 import no.nav.foreldrepenger.abakus.kobling.repository.LåsRepository;
-import no.nav.foreldrepenger.abakus.kodeverk.KodeverkRepository;
 import no.nav.foreldrepenger.abakus.kodeverk.YtelseType;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
@@ -25,10 +21,9 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 public class RegisterdataInnhentingTask extends KoblingTask {
     public static final String TASKTYPE = "registerdata.innhent";
 
-    private KodeverkRepository kodeverkRepository;
     private KoblingTjeneste koblingTjeneste;
     private InntektArbeidYtelseTjeneste iayTjeneste;
-    private Map<YtelseType, IAYRegisterInnhentingTjeneste> registerInnhentingTjeneste;
+    private Instance<IAYRegisterInnhentingTjeneste> innhentTjenester;
 
     RegisterdataInnhentingTask() {
     }
@@ -37,30 +32,19 @@ public class RegisterdataInnhentingTask extends KoblingTask {
     public RegisterdataInnhentingTask(LåsRepository låsRepository,
                                       KoblingTjeneste koblingTjeneste,
                                       InntektArbeidYtelseTjeneste iayTjeneste,
-                                      KodeverkRepository kodeverkRepository,
                                       @Any Instance<IAYRegisterInnhentingTjeneste> innhentingTjeneste) {
         super(låsRepository);
         this.koblingTjeneste = koblingTjeneste;
         this.iayTjeneste = iayTjeneste;
-        this.kodeverkRepository = kodeverkRepository;
-        this.registerInnhentingTjeneste = new HashMap<>();
-        innhentingTjeneste.forEach(innhenter -> populerMap(registerInnhentingTjeneste, innhenter));
-    }
-
-    private void populerMap(Map<YtelseType, IAYRegisterInnhentingTjeneste> map, IAYRegisterInnhentingTjeneste innhenter) {
-        YtelseType type = YtelseType.UDEFINERT;
-        if (innhenter.getClass().isAnnotationPresent(YtelseTypeRef.class)) {
-            type = kodeverkRepository.finn(YtelseType.class, innhenter.getClass().getAnnotation(YtelseTypeRef.class).value());
-        }
-        map.put(type, innhenter);
+        this.innhentTjenester = innhentingTjeneste;
     }
 
     private IAYRegisterInnhentingTjeneste finnInnhenter(YtelseType ytelseType) {
-        IAYRegisterInnhentingTjeneste innhenter = registerInnhentingTjeneste.get(ytelseType);
-        if (innhenter == null) {
+        final var tjenester = innhentTjenester.select(new YtelseTypeRef.FagsakYtelseTypeRefLiteral(ytelseType.getKode()));
+        if (tjenester.isAmbiguous() || tjenester.isUnsatisfied()) {
             throw new IllegalArgumentException("Finner ikke IAYRegisterInnhenter. Støtter ikke ytelsetype " + ytelseType);
         }
-        return innhenter;
+        return tjenester.get();
     }
 
     @Override
