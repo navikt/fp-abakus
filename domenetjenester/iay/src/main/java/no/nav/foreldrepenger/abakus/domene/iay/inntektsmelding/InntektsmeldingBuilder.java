81668de7eeadafc6e3b40934e16cdaf4298a165f
Marius Glittum <marius.glittum@nav.no>
2019-10-18 13:21:16 +0200

    Sikrer at inntektsmelding er kosistent

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/iay/inntektsmelding/InntektsmeldingBuilder.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/iay/inntektsmelding/InntektsmeldingBuilder.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.jboss.weld.exceptions.IllegalArgumentException;
+
 import no.nav.foreldrepenger.abakus.domene.iay.Arbeidsgiver;
 import no.nav.foreldrepenger.abakus.domene.iay.kodeverk.InntektsmeldingInnsendingsårsak;
 import no.nav.foreldrepenger.abakus.typer.Beløp;
@@ -16,6 +18,7 @@ import no.nav.vedtak.konfig.Tid;
 
 public class InntektsmeldingBuilder {
     private final InntektsmeldingEntitet kladd;
+    private boolean erBygget;
     private EksternArbeidsforholdRef eksternArbeidsforholdId;
 
     InntektsmeldingBuilder(InntektsmeldingEntitet kladd) {
@@ -27,6 +30,7 @@ public class InntektsmeldingBuilder {
     }
 
     public InntektsmeldingBuilder medArbeidsgiver(Arbeidsgiver arbeidsgiver) {
+        precondition();
         kladd.setArbeidsgiver(arbeidsgiver);
         return this;
     }
@@ -34,7 +38,7 @@ public class InntektsmeldingBuilder {
     public Arbeidsgiver getArbeidsgiver() {
         return kladd.getArbeidsgiver();
     }
-    
+
     public Optional<EksternArbeidsforholdRef> getEksternArbeidsforholdRef() {
         return Optional.ofNullable(eksternArbeidsforholdId);
     }
@@ -44,11 +48,13 @@ public class InntektsmeldingBuilder {
     }
 
     public InntektsmeldingBuilder medArbeidsforholdId(EksternArbeidsforholdRef arbeidsforholdId) {
+        precondition();
         this.eksternArbeidsforholdId = arbeidsforholdId;
         return this;
     }
 
     public InntektsmeldingBuilder medArbeidsforholdId(InternArbeidsforholdRef arbeidsforholdId) {
+        precondition();
         if (arbeidsforholdId != null) {
             // magic - hvis har ekstern referanse må også intern referanse være spesifikk
             if (arbeidsforholdId.getReferanse() == null && eksternArbeidsforholdId != null && eksternArbeidsforholdId.gjelderForSpesifiktArbeidsforhold()) {
@@ -60,89 +66,120 @@ public class InntektsmeldingBuilder {
         return this;
     }
     public InntektsmeldingBuilder medBeløp(BigDecimal verdi) {
+        precondition();
         kladd.setInntektBeløp(verdi == null ? null : new Beløp(verdi));
         return this;
     }
 
     public InntektsmeldingBuilder medJournalpostId(JournalpostId journalpostId) {
+        precondition();
         kladd.setJournalpostId(journalpostId);
         return this;
     }
 
     public InntektsmeldingBuilder medNærRelasjon(boolean nærRelasjon) {
+        precondition();
         kladd.setNærRelasjon(nærRelasjon);
         return this;
     }
 
     public InntektsmeldingBuilder medStartDatoPermisjon(LocalDate startPermisjon) {
+        precondition();
         kladd.setStartDatoPermisjon(startPermisjon);
         return this;
     }
 
     public InntektsmeldingBuilder medRefusjon(BigDecimal verdi, LocalDate opphører) {
+        precondition();
         kladd.setRefusjonBeløpPerMnd(verdi == null ? null : new Beløp(verdi));
         kladd.setRefusjonOpphører(opphører);
         return this;
     }
 
     public InntektsmeldingBuilder medRefusjon(BigDecimal verdi) {
+        precondition();
         kladd.setRefusjonBeløpPerMnd(verdi == null ? null : new Beløp(verdi));
         kladd.setRefusjonOpphører(Tid.TIDENES_ENDE);
         return this;
     }
 
     public InntektsmeldingBuilder medInnsendingstidspunkt(LocalDateTime innsendingstidspunkt) {
+        precondition();
         Objects.requireNonNull(innsendingstidspunkt, "innsendingstidspunkt");
         kladd.setInnsendingstidspunkt(innsendingstidspunkt);
         return this;
     }
 
     public InntektsmeldingBuilder medKanalreferanse(String kanalreferanse) {
+        precondition();
         kladd.setKanalreferanse(kanalreferanse);
         return this;
     }
-    
+
     public InntektsmeldingBuilder medMottattDato(LocalDate mottattDato) {
+        precondition();
         kladd.setMottattDato(mottattDato);
         return this;
     }
 
     public InntektsmeldingBuilder medKildesystem(String kildesystem) {
+        precondition();
         kladd.setKildesystem(kildesystem);
         return this;
     }
 
     public InntektsmeldingBuilder leggTil(NaturalYtelse naturalYtelse) {
+        precondition();
         kladd.leggTil(naturalYtelse);
         return this;
     }
 
     public InntektsmeldingBuilder leggTil(UtsettelsePeriode utsettelsePeriode) {
+        precondition();
         kladd.leggTil(utsettelsePeriode);
         return this;
     }
 
     public InntektsmeldingBuilder leggTil(Gradering gradering) {
+        precondition();
         kladd.leggTil(gradering);
         return this;
     }
 
     public InntektsmeldingBuilder leggTil(Refusjon refusjon) {
+        precondition();
         kladd.leggTil(refusjon);
         return this;
     }
 
     public InntektsmeldingBuilder medInntektsmeldingaarsak(InntektsmeldingInnsendingsårsak inntektsmeldingInnsendingsårsak) {
+        precondition();
         kladd.setInntektsmeldingInnsendingsårsak(inntektsmeldingInnsendingsårsak);
         return this;
     }
 
     public InntektsmeldingBuilder medInntektsmeldingaarsak(String inntektsmeldingInnsendingsårsak) {
+        precondition();
         return medInntektsmeldingaarsak(inntektsmeldingInnsendingsårsak == null ? null : new InntektsmeldingInnsendingsårsak(inntektsmeldingInnsendingsårsak));
     }
 
     public Inntektsmelding build() {
+        final var internRef = getInternArbeidsforholdRef();
+        if (internRef.isPresent()) {
+            // magic - hvis har ekstern referanse må også intern referanse være spesifikk
+            if ((eksternArbeidsforholdId != null && eksternArbeidsforholdId.gjelderForSpesifiktArbeidsforhold()) && internRef.get().getReferanse() == null) {
+                throw new IllegalArgumentException(
+                    "Begge referanser må gjelde spesifikke arbeidsforhold. " + " Ekstern: " + eksternArbeidsforholdId + ", Intern: " + internRef);
+            }
+        }
+        erBygget = true;
         return kladd;
+    }
+
+    private void precondition() {
+        if(erBygget) {
+            throw new IllegalStateException("Inntektsmelding objekt er allerede bygget, kan ikke modifisere nå. Returnerer kun : " + kladd);
+        }
     }
 
     public InntektsmeldingBuilder medJournalpostId(String journalpostId) {
