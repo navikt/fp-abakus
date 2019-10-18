56fd45a7882c5569f5b3eb3147fc7b7d014ac604
Marius Bækken Glittum <marius.glittum@nav.no>
2019-10-18 15:28:42 +0200

    Endrer filter og formål til enum som så sendes over. (#71)

---
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/komponenten/InntektsFilter.java
@@ -0,0 +1,24 @@
+package no.nav.foreldrepenger.abakus.registerdata.inntekt.komponenten;
+
+public enum InntektsFilter {
+
+    OPPTJENINGSGRUNNLAG("PensjonsgivendeA-Inntekt", InntektsFormål.FORMAAL_PGI),
+    BEREGNINGSGRUNNLAG("8-28", InntektsFormål.FORMAAL_FORELDREPENGER),
+    SAMMENLIGNINGSGRUNNLAG("8-30", InntektsFormål.FORMAAL_FORELDREPENGER); //$NON-NLS-1$
+
+    private String kode;
+    private InntektsFormål formål;
+
+    InntektsFilter(String kode, InntektsFormål formål) {
+        this.kode = kode;
+        this.formål = formål;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public InntektsFormål getFormål() {
+        return formål;
+    }
+}
