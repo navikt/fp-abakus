f46f2e2e67dab31bfdb38475d94d077b985e2efe
Marius Glittum <marius.glittum@nav.no>
2019-10-30 13:33:12 +0100

    Legger på delay på oppstart av taskmanager & kafka-consumer

--- web/src/main/java/no/nav/foreldrepenger/abakus/app/konfig/ApplicationContextListener.java
+++ web/src/main/java/no/nav/foreldrepenger/abakus/app/konfig/ApplicationContextListener.java
@@ -1,9 +1,12 @@
 package no.nav.foreldrepenger.abakus.app.konfig;
 
-import javax.inject.Inject;
+import javax.enterprise.inject.spi.CDI;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Starter interne applikasjontjenester
@@ -11,16 +14,38 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class ApplicationContextListener implements ServletContextListener {
 
-    @Inject
-    private ApplicationServiceStarter applicationServiceStarter; //NOSONAR - vil ikke fungere med constructor innjection
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationContextListener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        applicationServiceStarter.startServices();
+        // start denne async, logg til slutt når ferdig
+        Thread thread = new Thread(this::startServices, getClass().getSimpleName() + "-thread");
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    private void startServices() {
+        // Henter dependent instance og destroyer etterpå.
+        ApplicationServiceStarter serviceStarter = null;
+        try {
+            Thread.sleep(2000); // La verden gå litt videre får vi dumper ut
+            serviceStarter = CDI.current().select(ApplicationServiceStarter.class).get();
+            serviceStarter.startServices();
+        } catch (InterruptedException e) {
+            logger.warn("Feil under oppstart av services", e);
+            throw new RuntimeException(e);
+        } finally {
+            if (serviceStarter != null) {
+                CDI.current().destroy(serviceStarter);
+            }
+        }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        applicationServiceStarter.stopServices();
+        final var serviceStarter = CDI.current().select(ApplicationServiceStarter.class).get();
+        serviceStarter.stopServices();
     }
+
+
 }
