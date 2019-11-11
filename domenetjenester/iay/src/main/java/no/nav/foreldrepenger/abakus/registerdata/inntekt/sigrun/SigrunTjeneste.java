3db3519ce4bb26ca75fab62d335ce02968c0e807
Ole Jørgen Rishoff <Ole.Jorgen.Rishoff@nav.no>
2019-11-11 14:52:14 +0100

    TFP-1491 lagt til unleash

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/SigrunTjeneste.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/SigrunTjeneste.java
@@ -1,11 +1,13 @@
 package no.nav.foreldrepenger.abakus.registerdata.inntekt.sigrun;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import no.finn.unleash.Unleash;
 import no.nav.foreldrepenger.abakus.domene.iay.kodeverk.InntektspostType;
 import no.nav.foreldrepenger.abakus.typer.AktørId;
 import no.nav.vedtak.felles.integrasjon.sigrun.SigrunConsumer;
@@ -16,22 +18,26 @@ import no.nav.vedtak.felles.jpa.tid.DatoIntervallEntitet;
 
 @ApplicationScoped
 public class SigrunTjeneste {
-
     private SigrunConsumer sigrunConsumer;
+    private Unleash unleash;
 
     SigrunTjeneste() {
         //CDI
     }
 
     @Inject
-    public SigrunTjeneste(SigrunConsumer sigrunConsumer) {
+    public SigrunTjeneste(SigrunConsumer sigrunConsumer, Unleash unleash) {
         this.sigrunConsumer = sigrunConsumer;
+        this.unleash = unleash;
     }
 
     public Map<DatoIntervallEntitet, Map<InntektspostType, BigDecimal>> beregnetSkatt(AktørId aktørId) {
         SigrunResponse beregnetskatt = sigrunConsumer.beregnetskatt(Long.valueOf(aktørId.getId()));
-        SigrunSummertSkattegrunnlagResponse summertSkattegrunnlag = sigrunConsumer.summertSkattegrunnlag(Long.valueOf(aktørId.getId()));
-
-        return SigrunTilInternMapper.mapFraSigrunTilIntern(beregnetskatt.getBeregnetSkatt(), summertSkattegrunnlag.getSummertskattegrunnlagMap());
+        if (unleash.isEnabled("fpsak.sigrun.summertskattegrunnlag")) {
+            SigrunSummertSkattegrunnlagResponse summertSkattegrunnlag = sigrunConsumer.summertSkattegrunnlag(Long.valueOf(aktørId.getId()));
+            return SigrunTilInternMapper.mapFraSigrunTilIntern(beregnetskatt.getBeregnetSkatt(), summertSkattegrunnlag.getSummertskattegrunnlagMap());
+        } else {
+            return SigrunTilInternMapper.mapFraSigrunTilIntern(beregnetskatt.getBeregnetSkatt(), Collections.emptyMap());
+        }
     }
 }
