15b3408d14b016cf38107db405f2cf3587d12eed
Ole Jørgen Rishoff <Ole.Jorgen.Rishoff@nav.no>
2019-11-11 14:27:14 +0100

    TFP-1491 lagt til en test til

--- domenetjenester/iay/src/test/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/SigrunTilInternMapperTest.java
+++ domenetjenester/iay/src/test/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/SigrunTilInternMapperTest.java
@@ -52,4 +52,21 @@ public class SigrunTilInternMapperTest {
 
         assertThat(map.get(DatoIntervallEntitet.fraOgMedTilOgMed(førsteDagIÅr, sisteDagIÅr)).get(InntektspostType.LØNN)).isEqualByComparingTo(new BigDecimal(5000));
     }
+
+    @Test
+    public void skal_mappe_og_beregne_slå_sammen_lønn_fra_beregent_med_lønn_fra_summertskattegrunnlag_og_håndtere_skatteoppgjørsdato() {
+        LocalDate iDag = LocalDate.now();
+        Map<Year, List<BeregnetSkatt>> beregnet = new HashMap<>();
+        Map<Year, Optional<SSGResponse>> summertskattegrunnlag = new HashMap<>();
+
+        Year iÅr = Year.of(iDag.getYear());
+        beregnet.put(iÅr, List.of(new BeregnetSkatt(TekniskNavn.PERSONINNTEKT_LØNN.getKode(), "5000"), new BeregnetSkatt(TekniskNavn.SKATTEOPPGJØRSDATO.getKode(), "2018-10-04")));
+        summertskattegrunnlag.put(iÅr, Optional.of(new SSGResponse(List.of(), List.of(new SSGGrunnlag(TekniskNavn.LØNNSINNTEKT_MED_TRYGDEAVGIFTSPLIKT_OMFATTET_AV_LØNNSTREKKORDNINGEN.getKode(), "5000")), null)));
+
+        Map<DatoIntervallEntitet, Map<InntektspostType, BigDecimal>> map = SigrunTilInternMapper.mapFraSigrunTilIntern(beregnet, summertskattegrunnlag);
+        LocalDate førsteDagIÅr = LocalDate.of(iÅr.getValue(), 1, 1);
+        LocalDate sisteDagIÅr = LocalDate.of(iÅr.getValue(), 12, 31);
+
+        assertThat(map.get(DatoIntervallEntitet.fraOgMedTilOgMed(førsteDagIÅr, sisteDagIÅr)).get(InntektspostType.LØNN)).isEqualByComparingTo(new BigDecimal(10000));
+    }
 }
