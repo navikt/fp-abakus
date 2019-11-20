dd099e313dd2d1affc368269084ac6bc3d1d562e
Marius Glittum <marius.glittum@nav.no>
2019-11-20 11:05:56 +0100

    Endrer fra repository til cache

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/virksomhet/VirksomhetRepositoryImpl.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/virksomhet/VirksomhetRepositoryImpl.java
@@ -4,77 +4,27 @@ import java.util.Objects;
 import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.LockModeType;
-import javax.persistence.PersistenceException;
-import javax.persistence.TypedQuery;
 
-import org.hibernate.exception.ConstraintViolationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import no.nav.vedtak.felles.jpa.HibernateVerktøy;
-import no.nav.vedtak.felles.jpa.VLPersistenceUnit;
+import no.nav.vedtak.util.LRUCache;
 
 @ApplicationScoped
 public class VirksomhetRepositoryImpl implements VirksomhetRepository {
 
-    private EntityManager entityManager;
-    private Logger logger = LoggerFactory.getLogger(VirksomhetRepositoryImpl.class);
+    private LRUCache<String, Virksomhet> cache;
 
     public VirksomhetRepositoryImpl() {
-        // CDI
-    }
-
-    @Inject
-    public VirksomhetRepositoryImpl(@VLPersistenceUnit EntityManager entityManager) {
-        Objects.requireNonNull(entityManager, "entityManager"); //$NON-NLS-1$
-        this.entityManager = entityManager;
+        this.cache = new LRUCache<>(1000, 60 * 1000 * 15);
     }
 
     @Override
     public Optional<Virksomhet> hent(String orgnr) {
-        final Optional<VirksomhetEntitet> virksomhetEntitet = getVirksomhet(orgnr, false);
-        if (virksomhetEntitet.isPresent()) {
-            VirksomhetEntitet value = virksomhetEntitet.get();
-            entityManager.detach(value);
-            return Optional.of(value);
-        }
-        return Optional.empty();
-    }
-
-    private Optional<VirksomhetEntitet> getVirksomhet(String orgnr, boolean forWrite) {
-        final TypedQuery<VirksomhetEntitet> query = entityManager.createQuery("FROM Virksomhet WHERE orgnr = :orgnr", VirksomhetEntitet.class);
-        query.setParameter("orgnr", orgnr);
-        if (forWrite) {
-            query.setLockMode(LockModeType.PESSIMISTIC_WRITE);
-        }
-        return HibernateVerktøy.hentUniktResultat(query);
+        return Optional.ofNullable(cache.get(orgnr));
     }
 
     @Override
     public void lagre(Virksomhet virksomhet) {
-        Optional<VirksomhetEntitet> virksomhet1 = getVirksomhet(virksomhet.getOrgnr(), true);
-        VirksomhetEntitet.Builder builder = virksomhet1.map(VirksomhetEntitet.Builder::new)
-            .orElse(new VirksomhetEntitet.Builder(virksomhet));
-        builder.medRegistrert(virksomhet.getRegistrert())
-            .medOppstart(virksomhet.getOppstart())
-            .medNavn(virksomhet.getNavn())
-            .medOrganisasjonstype(virksomhet.getOrganisasjonstype())
-            .oppdatertOpplysningerNå();
-        virksomhet = builder.build();
-        entityManager.persist(virksomhet);
-        try {
-            entityManager.flush();
-            entityManager.detach(virksomhet);
-        } catch (PersistenceException exception) {
-            if (exception.getCause() instanceof ConstraintViolationException) {
-                logger.info("Prøver å lagre duplikat virksomhet={}.", virksomhet);
-                throw new VirksomhetAlleredeLagretException();
-            }
-            throw exception;
-        }
+        Objects.requireNonNull(virksomhet, "virksomhet kan ikke være null");
+        cache.put(virksomhet.getOrgnr(), virksomhet);
     }
 
 }
