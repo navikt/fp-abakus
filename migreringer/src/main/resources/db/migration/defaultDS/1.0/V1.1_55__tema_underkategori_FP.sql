c2e0cc3bdc8c076d77a8eb3a740c61d99d98893b
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-11-21 16:49:55 +0100

    FIX legger til TemaUnderkategori FP

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_55__tema_underkategori_FP.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO KODELISTE (ID, KODEVERK, KODE, NAVN, BESKRIVELSE, SPRAK, GYLDIG_FOM, GYLDIG_TOM) VALUES (nextval('SEQ_KODELISTE') ,'TEMA_UNDERKATEGORI', 'FP','Foreldrepenger','','NB', to_date('01.01.2000', 'dd.mm.yyyy'), to_date('31.12.9999', 'dd.mm.yyyy'));
