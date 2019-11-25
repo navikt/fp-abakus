4ec0c312d73619a3dbbf2e0fc774cce720fdcae5
Petter Ekern <petter.ekern@nav.no>
2019-11-25 08:27:21 +0100

    FIX: bytter port for å unngå konflikt med fpformidling

--- web/src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevKonfigurasjon.java
+++ web/src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevKonfigurasjon.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.abakus.jetty;
 
 public class JettyDevKonfigurasjon extends JettyWebKonfigurasjon {
     private static final int SSL_SERVER_PORT = 8043;
-    private static final int DEV_SERVER_PORT = 8010;
+    private static final int DEV_SERVER_PORT = 8015;
 
     public JettyDevKonfigurasjon() {
         super(DEV_SERVER_PORT);
