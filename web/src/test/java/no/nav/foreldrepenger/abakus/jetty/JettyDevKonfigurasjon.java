0ddb67ee7e46246cbd10e5621c906dcaaf82f0f1
Mayu-Thiyaga <39523522+Mayu-Thiyaga@users.noreply.github.com>
2019-12-03 14:11:43 +0100

    En annen applikasjon bruker 8043.

--- web/src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevKonfigurasjon.java
+++ web/src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevKonfigurasjon.java
@@ -1,7 +1,7 @@
 package no.nav.foreldrepenger.abakus.jetty;
 
 public class JettyDevKonfigurasjon extends JettyWebKonfigurasjon {
-    private static final int SSL_SERVER_PORT = 8043;
+    private static final int SSL_SERVER_PORT = 8042;
     private static final int DEV_SERVER_PORT = 8015;
 
     public JettyDevKonfigurasjon() {
