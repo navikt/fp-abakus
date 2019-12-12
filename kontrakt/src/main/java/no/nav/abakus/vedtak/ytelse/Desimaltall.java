ee90d210ba5bcd3f6f96547d069a6ba7027837e5
Petter Ekern <43809623+pekern@users.noreply.github.com>
2019-12-12 14:08:03 +0100

    FIX: Legger til tom konstruktør (#217)

--- kontrakt/src/main/java/no/nav/abakus/vedtak/ytelse/Desimaltall.java
+++ kontrakt/src/main/java/no/nav/abakus/vedtak/ytelse/Desimaltall.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Desimaltall {
 
+    public Desimaltall() {
+    }
+
     @JsonProperty("verdi")
     private BigDecimal verdi;
 
