e38611a6874c2d8af55cb89fdc9adb8b26790730
Michal J. Sladek <michal.sladek@nav.no>
2019-12-19 10:49:48 +0100

    FIX: fikset docker-compose oppsett

--- update-versions.sh
+++ update-versions.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 
-export VTP_VERSION="repo-fra-laptop-tunnel:14129/vtp:"$(git ls-remote --tags https://github.com/navikt/vtp.git | sort -t '/' -k 3 -V | tail -2 | head -1 | sed 's/.*refs\/tags\/\([a-z.0-9_]*$\)/\1/g')
-echo VTP_IMAGE=${VTP_VERSION} >> .env
-
+echo VTP_IMAGE=docker.pkg.github.com/navikt/vtp/vtp > .env
 echo POSTGRES_IMAGE=postgres:12 >> .env
 
 
-echo ".env fil opprettet - Klart for docker-compose up abakus [oracle]"
+echo ".env fil opprettet - Klart for docker-compose up"
