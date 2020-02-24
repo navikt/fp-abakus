6d821ef7733d9ed59d67b6930964e901dd00f83f
Marius Glittum <marius.glittum@nav.no>
2020-02-24 13:44:41 +0100

    Retter feil i callback dto og endrer type på referanse til UUID

--- kontrakt/src/main/java/no/nav/abakus/callback/registerdata/ReferanseDto.java
+++ kontrakt/src/main/java/no/nav/abakus/callback/registerdata/ReferanseDto.java
@@ -1,5 +1,8 @@
 package no.nav.abakus.callback.registerdata;
 
+import java.util.UUID;
+
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -11,14 +14,14 @@ public class ReferanseDto {
     public static final String UUID_REGEX = "\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b";
 
     @NotNull
-    @Pattern(regexp = UUID_REGEX)
-    private String referanse;
+    @Valid
+    private UUID referanse;
 
-    public String getReferanse() {
+    public UUID getReferanse() {
         return referanse;
     }
 
-    public void setReferanse(String referanse) {
+    public void setReferanse(UUID referanse) {
         this.referanse = referanse;
     }
 }
