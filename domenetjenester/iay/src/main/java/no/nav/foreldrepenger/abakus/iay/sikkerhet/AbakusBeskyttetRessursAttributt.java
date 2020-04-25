be66eb8263055bf8b98a70dae57c3502d91ee678
Michal J. Sladek <mrsladek@users.noreply.github.com>
2020-04-25 17:05:48 +0200

    FIX: Type i abac resource_type definitions. (#536)

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/iay/sikkerhet/AbakusBeskyttetRessursAttributt.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/iay/sikkerhet/AbakusBeskyttetRessursAttributt.java
@@ -5,7 +5,7 @@ public final class AbakusBeskyttetRessursAttributt {
     public static final String GRUNNLAG = "no.nav.abac.attributter.resource.duplo.abakus.grunnlag";
     public static final String ARBEIDSFORHOLD = "no.nav.abac.attributter.resource.duplo.abakus.arbeidsforhold";
     public static final String INNTEKSTMELDING = "no.nav.abac.attributter.resource.duplo.abakus.inntektsmelding";
-    public static final String SØKNAD = "no.nav.abac.attributter.duplo.resource.abakus.soeknad";
+    public static final String SØKNAD = "no.nav.abac.attributter.resource.duplo.abakus.soeknad";
     public static final String REGISTERDATA = "no.nav.abac.attributter.resource.duplo.abakus.registerdata";
-    public static final String DRIFT = "no.nav.abac.attributter.duplo.resource.abakus.drift";
+    public static final String DRIFT = "no.nav.abac.attributter.resource.duplo.abakus.drift";
 }
