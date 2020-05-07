4101d5be735aef677f01e7cad64a0d6bbed6a51e
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-07 10:23:17 +0200

    fix: fjerner konfig logging ved oppstart (#604)

--- web/src/main/java/no/nav/foreldrepenger/abakus/app/startupinfo/AppStartupInfoLogger.java
+++ web/src/main/java/no/nav/foreldrepenger/abakus/app/startupinfo/AppStartupInfoLogger.java
@@ -33,15 +33,10 @@ class AppStartupInfoLogger {
     private static final String OPPSTARTSINFO = "OPPSTARTSINFO";
     private static final String HILITE_SLUTT = "********";
     private static final String HILITE_START = HILITE_SLUTT;
-    private static final String KONFIGURASJON = "Konfigurasjon";
     private static final String SELFTEST = "Selftest";
     private static final String APPLIKASJONENS_STATUS = "Applikasjonens status";
-    private static final String SYSPROP = "System property";
-    private static final String ENVVAR = "Env. var";
     private static final String START = "start:";
     private static final String SLUTT = "slutt.";
-    private static final String SKIP_LOG_SYS_PROPS = "skipLogSysProps";
-    private static final String SKIP_LOG_ENV_VARS = "skipLogEnvVars";
     private static final String TRUE = "true";
     private Selftests selftests;
     /**
@@ -61,7 +56,7 @@ class AppStartupInfoLogger {
     void logAppStartupInfo() {
         log(HILITE_START + " " + OPPSTARTSINFO + " " + START + " " + HILITE_SLUTT);
         logVersjoner();
-        logKonfigurasjon();
+     
         logSelftest();
         log(HILITE_START + " " + OPPSTARTSINFO + " " + SLUTT + " " + HILITE_SLUTT);
 
@@ -79,32 +74,6 @@ class AppStartupInfoLogger {
         log("Bibliotek: Weld: {}", Formats.version(null));
         log("Bibliotek: CDI: {}", CDI.class.getPackage().getImplementationVendor() + ":" + CDI.class.getPackage().getSpecificationVersion());
         log("Bibliotek: Resteasy: {}", Query.class.getPackage().getImplementationVersion()); // tilfeldig valgt Resteasy klasse
-    }
-
-    private void logKonfigurasjon() {
-        log(KONFIGURASJON + " " + START);
-
-        SystemPropertiesHelper sysPropsHelper = SystemPropertiesHelper.getInstance();
-        boolean skipSysProps = TRUE.equalsIgnoreCase(System.getProperty(SKIP_LOG_SYS_PROPS));
-        boolean skipEnvVars = TRUE.equalsIgnoreCase(System.getProperty(SKIP_LOG_ENV_VARS));
-
-        if (!skipSysProps) {
-            SortedMap<String, String> sysPropsMap = sysPropsHelper.filteredSortedProperties();
-            String sysPropFormat = SYSPROP + ": {}={}";
-            for (Entry<String, String> entry : sysPropsMap.entrySet()) {
-                log(sysPropFormat, LoggerUtils.removeLineBreaks(entry.getKey()), LoggerUtils.removeLineBreaks(entry.getValue()));
-            }
-        }
-
-        if (!skipEnvVars) {
-            SortedMap<String, String> envVarsMap = sysPropsHelper.filteredSortedEnvVars();
-            for (Entry<String, String> entry : envVarsMap.entrySet()) {
-                String envVarFormat = ENVVAR + ": {}={}";
-                log(envVarFormat, LoggerUtils.removeLineBreaks(entry.getKey()), LoggerUtils.removeLineBreaks(entry.getValue()));
-            }
-        }
-
-        log(KONFIGURASJON + " " + SLUTT);
     }
 
     private void logSelftest() {
