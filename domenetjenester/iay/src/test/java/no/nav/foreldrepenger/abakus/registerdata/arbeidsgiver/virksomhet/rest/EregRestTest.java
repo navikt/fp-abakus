5ab18882e9949a46d71ccea130eb79b59b2fa545
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-06-11 09:41:54 +0200

    TFP-3397 fjern virksomhet-tabell etter bytte til ereg rs (#695)

--- domenetjenester/iay/src/test/java/no/nav/foreldrepenger/abakus/registerdata/arbeidsgiver/virksomhet/rest/EregRestTest.java
+++ domenetjenester/iay/src/test/java/no/nav/foreldrepenger/abakus/registerdata/arbeidsgiver/virksomhet/rest/EregRestTest.java
@@ -1,11 +1,30 @@
 package no.nav.foreldrepenger.abakus.registerdata.arbeidsgiver.virksomhet.rest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class EregRestTest {
 
+    private static ObjectMapper mapper;
+
+    @BeforeClass
+    public static void setup() {
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
     @Test
-    public void roundtrip_mapping_dto_til_grunnlag_til_dto() {
+    public void mapping_dto_til_grunnlag_til_dto() throws IOException {
         // Arrange
         String json ="{\n \"organisasjonsnummer\":\"999999999\",\n" +
             " \"type\":\"Virksomhet\",\n" +
@@ -18,13 +37,14 @@ public class EregRestTest {
             " \"virksomhetDetaljer\":null\n" +
             "}";
 
-        var org = JsonMapper.fromJson(json, OrganisasjonEReg.class);
-        var navn = org.getNavn();
-        var orgnr = org.getOrganisasjonsnummer();
+        var org = fromJson(json, OrganisasjonEReg.class);
+        assertThat(org.getNavn()).isEqualTo("MIN BEDRIFT AVD DER JEG BOR");
+        assertThat(org.getOrganisasjonsnummer()).isEqualTo("999999999");
+        assertThat(org.getType()).isEqualTo(OrganisasjonstypeEReg.VIRKSOMHET);
     }
 
     @Test
-    public void roundtrip_mapping_jurdisk_enhet() {
+    public void mapping_jurdisk_enhet() throws IOException {
         // Arrange
         String json ="{\n" +
             "  \"organisasjonsnummer\": \"999999999\",\n" +
@@ -55,8 +75,15 @@ public class EregRestTest {
             "  ]\n" +
             "}";
 
-        var org = JsonMapper.fromJson(json, JuridiskEnhetVirksomheter.class);
-        var virksomheter = org.getDriverVirksomheter();
-        var orgnr = org.getOrganisasjonsnummer();
+        var org = fromJson(json, JuridiskEnhetVirksomheter.class);
+        assertThat(org.getDriverVirksomheter()).hasSize(2);
+        assertThat(org.getEksaktVirksomhetForDato(LocalDate.now())).hasSize(1);
+        assertThat(org.getEksaktVirksomhetForDato(LocalDate.now()).get(0)).isEqualTo("999999997");
+        assertThat(org.getOrganisasjonsnummer()).isEqualTo("999999999");
+        assertThat(org.getType()).isEqualTo(OrganisasjonstypeEReg.JURIDISK_ENHET);
+    }
+
+    private static <T> T fromJson(String json, Class<T> clazz) throws IOException {
+        return mapper.readerFor(clazz).readValue(json);
     }
 }
