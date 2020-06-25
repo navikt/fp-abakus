02aef50a4144eccc086e84075f8e2c0a0b44f05f
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-06-25 15:51:27 +0200

    Byttet HttpKlient i SpokelseKlient. Config for Azure og Spøkelse i prod & dev.

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/spokelse/SpokelseKlient.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/spokelse/SpokelseKlient.java
@@ -4,15 +4,16 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import no.nav.vedtak.felles.integrasjon.rest.OAuth2RestClient;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 import no.nav.vedtak.konfig.KonfigVerdi;
 
 @ApplicationScoped
@@ -22,13 +23,25 @@ public class SpokelseKlient {
 
     private static final Logger LOG = LoggerFactory.getLogger(SpokelseKlient.class);
 
-    private OidcRestClient restClient;
     private URI uri;
     private String uriString;
+    private OAuth2RestClient restClient;
 
     @Inject
-    public SpokelseKlient(OidcRestClient restClient, @KonfigVerdi(value = "spokelse.grunnlag.url", defaultVerdi = DEFAULT_URI) URI uri) {
-        this.restClient = restClient;
+    public SpokelseKlient(
+        @KonfigVerdi(value = "SPOKELSE_GRUNNLAG_URL", defaultVerdi = DEFAULT_URI) URI uri,
+        @KonfigVerdi(value = "SPOKELSE_GRUNNLAG_SCOPES") String scopesCsv,
+        @KonfigVerdi(value = "AZURE_CLIENT_ID") String clientId,
+        @KonfigVerdi(value = "AZURE_CLIENT_SECRET") String clientSecret,
+        @KonfigVerdi(value = "AZURE_V2_TOKEN_ENDPOINT") URI tokenEndpoint,
+        @KonfigVerdi(value = "AZURE_HTTP_PROXY", required = false) URI httpProxy) {
+        this.restClient = OAuth2RestClient.builder()
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .scopes(scopesFraCsv(scopesCsv))
+            .tokenEndpoint(tokenEndpoint)
+            .tokenEndpointProxy(httpProxy)
+            .build();
         this.uri = uri;
         this.uriString = uri.toString();
     }
@@ -49,5 +62,9 @@ public class SpokelseKlient {
             LOG.info("abakus spokelse Feil ved oppslag mot {}, returnerer ingen grunnlag", uriString, e);
             return Collections.emptyList();
         }
+    }
+
+    private static Set<String> scopesFraCsv(String scopesCsv) {
+        return Set.of(scopesCsv.replace(" ", "").split(","));
     }
 }
--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 
-echo VTP_IMAGE=docker.pkg.github.com/navikt/vtp/vtp > .env
+echo VTP_IMAGE=docker.pkg.github.com/navikt/vtp/vtp >> .env
 echo POSTGRES_IMAGE=postgres:12 >> .env
+echo AZURE_MOCK_IMAGE=docker.pkg.github.com/navikt/azure-mock/azure-mock:latest >> .env
 
 
 echo ".env fil opprettet - Klart for docker-compose up"
