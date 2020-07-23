c97de07c7da339c87251230559360812aca9d284
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-07-23 10:26:02 +0200

    Fix: Feature for å skru av kall til inntektskomponenten (#784)
    * Fix: Feature for å skru av kall til inntektskomponenten
    for å unngå at prossesstasker skal henge og låse systemet
    
    * Fix: Feature for å skru av kall til inntektskomponenten
    for å unngå at prossesstasker skal henge og låse systemet

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/komponenten/InntektFeil.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/komponenten/InntektFeil.java
@@ -20,8 +20,11 @@ public interface InntektFeil extends DeklarerteFeil {
     @TekniskFeil(feilkode = "FP-263743", feilmelding = "Feil ved opprettelse av request mot inntektstjenesten.", logLevel = LogLevel.ERROR)
     Feil feilVedOpprettelseAvInntektRequest(DatatypeConfigurationException e);
 
-    @IntegrasjonFeil(feilkode = "FP-824246", feilmelding = "Feil ved kall til inntektstjenesten. Meld til #di_team_registre og #produksjonshendelser hvis dette skjer over lengre tidsperiode.", logLevel = LogLevel.ERROR)
+    @IntegrasjonFeil(feilkode = "FP-824246", feilmelding = "Feil ved kall til inntektstjenesten. Meld til #team_registre og #produksjonshendelser hvis dette skjer over lengre tidsperiode.", logLevel = LogLevel.ERROR)
     Feil feilVedKallTilInntekt(Exception e);
+
+    @IntegrasjonFeil(feilkode = "FP-824246", feilmelding = "Inntektstjeneesten er nede, og kall dit er skrudd av for at prosesstasker ikke skal henge og låse fpsak. Når inntektstjeneten er oppe igjen, endre i unleash fpabakus.disable.kall.inntektskomponenten.", logLevel = LogLevel.ERROR)
+    Feil skruddAvKallTilInntekt();
 
     @TekniskFeil(feilkode = "FP-722674", feilmelding = "Kunne ikke serialisere response fra Inntektskomponenten.", logLevel = LogLevel.ERROR)
     Feil kunneIkkeSerialisereResponse(JAXBException e);
