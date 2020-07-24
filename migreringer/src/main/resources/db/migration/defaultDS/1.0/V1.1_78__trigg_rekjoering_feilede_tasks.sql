1f6c1e3e342a60e003f5ea064383b6137d06d94f
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2020-07-24 14:06:52 +0200

    Fikset sql

--- migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_78__trigg_rekjoering_feilede_tasks.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_78__trigg_rekjoering_feilede_tasks.sql
@@ -1,5 +1,1 @@
-update PROSESS_TASK
-set
-  neste_kjoering_etter = current_timestamp
-where
-  task_type = 'retry.feilendeTasks' and status = 'KLAR';
+update PROSESS_TASK set neste_kjoering_etter = current_timestamp where task_type = 'retry.feilendeTasks' and status = 'KLAR';
