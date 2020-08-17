d619916fd55b70ee3aae06de918f833553409b65
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-08-17 22:10:23 +0200

    fix: håndter at noen kanalreferanser kan være null i joark pga manglende eksternreferanse satt (#839)

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/iay/inntektsmelding/ForvaltningOppdaterKanalReferanseTask.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/iay/inntektsmelding/ForvaltningOppdaterKanalReferanseTask.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
 
 import org.hibernate.jpa.QueryHints;
 import org.slf4j.Logger;
@@ -38,38 +39,43 @@ public class ForvaltningOppdaterKanalReferanseTask implements ProsessTaskHandler
     @SuppressWarnings("unchecked")
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
-        var max = Integer.valueOf(prosessTaskData.getProperties().getProperty("max.antall", "5"));
+        var max = Integer.valueOf(prosessTaskData.getProperties().getProperty("max.antall", "10"));
 
-        Stream<String> stream = entityManager.createNativeQuery("select distinct journalpost_id from IAY_INNTEKTSMELDING where kanalreferanse is null")
+        Stream<Tuple> stream = entityManager.createNativeQuery("select distinct journalpost_id, endret_tid from IAY_INNTEKTSMELDING where kanalreferanse is null order by endret_tid nulls first", Tuple.class)
             .setMaxResults(max)
             .setHint(QueryHints.HINT_FETCH_SIZE, max)
             .getResultStream();
 
-        var journalpostIder = stream.limit(max).collect(Collectors.toList());
+        var journalpostIder = stream.map(t -> t.get("journalpost_id", String.class)).limit(max).collect(Collectors.toList());
 
-        if (journalpostIder.isEmpty()) {
-            log.warn("Ingen journalpost ider uten kanalreferanse funnet. Forvaltningsoppgaven kan avsluttes.");
-            return;
-        }
 
+        boolean funnet = false;
         for (var jourId : journalpostIder) {
             JournalpostQuery query = new JournalpostQuery(jourId);
             var journalpostInfo = safTjeneste.hentJournalpostInfo(query);
 
             String kanalreferanse = journalpostInfo.getEksternReferanseId();
 
-            if ("INGEN".equals(kanalreferanse)) {
+            if (kanalreferanse == null || "INGEN".equals(kanalreferanse)) {
                 log.warn("Fant dodgy kanalreferanse {} for journalpost: {}", kanalreferanse, jourId);
-            } else if (kanalreferanse != null) {
-                log.info("Fant kanalreferanse {} for journalpost {}", kanalreferanse, jourId);
-                entityManager.createNativeQuery("update IAY_INNTEKTSMELDING set kanalreferanse=:kanalreferanse where journalpost_id=:journalpostId AND kanalreferanse IS NULL")
-                    .setParameter("kanalreferanse", kanalreferanse)
-                    .setParameter("journalpostId", jourId)
-                    .executeUpdate();
+                kanalreferanse = null;
             } else {
-                log.warn("Fant ikke kanalreferanse for journalpost: {}", jourId);
+                funnet = true;
+                log.info("Fant kanalreferanse {} for journalpost {}", kanalreferanse, jourId);
             }
+            entityManager.createNativeQuery("update IAY_INNTEKTSMELDING"
+                + " set kanalreferanse=:kanalreferanse, endret_tid=current_timestamp at time zone 'UTC' "
+                + "where journalpost_id=:journalpostId AND kanalreferanse IS NULL")
+                .setParameter("kanalreferanse", kanalreferanse)
+                .setParameter("journalpostId", jourId)
+                .executeUpdate();
         }
+        
+        if (!funnet) {
+            log.warn("Journalpostider ({}) uten kanalreferanse funnet, eller alle var null. Forvaltningsoppgaven kan avsluttes.", journalpostIder.size());
+            return;
+        }
+        
 
     }
 }
