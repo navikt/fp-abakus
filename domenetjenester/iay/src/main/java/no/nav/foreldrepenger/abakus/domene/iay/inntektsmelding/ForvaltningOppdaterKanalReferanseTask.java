c657ae8849f1ba2aea86d70fb81ebad1d22ddecf
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-08-17 08:32:31 +0200

    fix: TSF-2878 populerer kanalreferanse der det mangler i abakus i egen jobb (#823)

---
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/domene/iay/inntektsmelding/ForvaltningOppdaterKanalReferanseTask.java
@@ -0,0 +1,72 @@
+package no.nav.foreldrepenger.abakus.domene.iay.inntektsmelding;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import org.hibernate.jpa.QueryHints;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.vedtak.felles.integrasjon.saf.SafTjeneste;
+import no.nav.vedtak.felles.integrasjon.saf.graphql.JournalpostQuery;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+
+@ApplicationScoped
+@ProsessTask(ForvaltningOppdaterKanalReferanseTask.TASK_TYPE)
+public class ForvaltningOppdaterKanalReferanseTask implements ProsessTaskHandler {
+    private static final Logger log = LoggerFactory.getLogger(ForvaltningOppdaterKanalReferanseTask.class);
+    public static final String TASK_TYPE = "forvaltning.oppdaterKanalreferanse";
+    private EntityManager entityManager;
+    private SafTjeneste safTjeneste;
+
+    public ForvaltningOppdaterKanalReferanseTask() {
+        // proxy
+    }
+
+    @Inject
+    public ForvaltningOppdaterKanalReferanseTask(SafTjeneste safTjeneste, EntityManager entityManager) {
+        this.safTjeneste = safTjeneste;
+        this.entityManager = entityManager;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void doTask(ProsessTaskData prosessTaskData) {
+        var max = Integer.valueOf(prosessTaskData.getProperties().getProperty("max.antall", "5"));
+
+        Stream<String> stream = entityManager.createNativeQuery("select distinct journalpost_id from IAY_INNTEKTSMELDING where kanalreferanse is null")
+            .setMaxResults(max)
+            .setHint(QueryHints.HINT_FETCH_SIZE, max)
+            .getResultStream();
+
+        var journalpostIder = stream.limit(max).collect(Collectors.toList());
+
+        if (journalpostIder.isEmpty()) {
+            throw new IllegalStateException("Ingen journalpost ider uten kanalreferanse funnet. Forvaltningsoppgaven kan avsluttes.");
+        }
+
+        for (var jourId : journalpostIder) {
+            JournalpostQuery query = new JournalpostQuery(jourId);
+            var journalpostInfo = safTjeneste.hentJournalpostInfo(query);
+
+            String kanalreferanse = journalpostInfo.getEksternReferanseId();
+
+            if (kanalreferanse != null) {
+                log.info("Fant kanalreferanse {} for journalpost {}", kanalreferanse, jourId);
+                entityManager.createNativeQuery("update IAY_INNTEKTSMELDING set kanalreferanse=:kanalreferanse where journalpost_id=:journalpostId AND kanalreferanse IS NULL")
+                    .setParameter("kanalreferanse", kanalreferanse)
+                    .setParameter("journalpostId", jourId)
+                    .executeUpdate();
+            } else {
+                log.warn("Fant ikke kanalreferanse for journalpost: {}", jourId);
+            }
+        }
+
+    }
+}
