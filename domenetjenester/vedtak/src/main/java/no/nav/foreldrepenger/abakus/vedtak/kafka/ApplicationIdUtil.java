3ac69a658fbb76bc5ae054c73b7b36dc02ed377e
Michal J. Sladek <michal.sladek@nav.no>
2020-09-07 10:22:28 +0200

    Fikset applicationId

--- domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/abakus/vedtak/kafka/ApplicationIdUtil.java
+++ domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/abakus/vedtak/kafka/ApplicationIdUtil.java
@@ -1,11 +1,19 @@
 package no.nav.foreldrepenger.abakus.vedtak.kafka;
 
+import no.nav.vedtak.util.env.Environment;
+
 public final class ApplicationIdUtil {
+
+    private static final Environment ENV = Environment.current();
 
     private ApplicationIdUtil() {
     }
 
     public static String get() {
-        return System.getProperty("nais.app.name", "fpabakus");
+        String prefix = ENV.getProperty("nais.app.name", "fpabakus");
+        if (ENV.isProd()) {
+            return prefix + "-default";
+        }
+        return prefix + "-" + ENV.namespace();
     }
 }
