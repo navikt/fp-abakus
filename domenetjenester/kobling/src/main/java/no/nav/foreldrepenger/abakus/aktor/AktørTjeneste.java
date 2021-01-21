e0d2c511ca5c01c5fe42d90a7312ccaf442f225f
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2021-01-21 22:50:53 +0100

    NOJIRA sende med riktig tema i header

--- domenetjenester/kobling/src/main/java/no/nav/foreldrepenger/abakus/aktor/AktørTjeneste.java
+++ domenetjenester/kobling/src/main/java/no/nav/foreldrepenger/abakus/aktor/AktørTjeneste.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.abakus.aktor;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -18,8 +19,11 @@ import no.nav.pdl.IdentInformasjonResponseProjection;
 import no.nav.pdl.Identliste;
 import no.nav.pdl.IdentlisteResponseProjection;
 import no.nav.vedtak.exception.VLException;
+import no.nav.vedtak.felles.integrasjon.pdl.JerseyPdlKlient;
+import no.nav.vedtak.felles.integrasjon.pdl.Pdl;
 import no.nav.vedtak.felles.integrasjon.pdl.PdlKlient;
-import no.nav.vedtak.felles.integrasjon.pdl.Tema;
+import no.nav.vedtak.felles.integrasjon.rest.StsAccessTokenConfig;
+import no.nav.vedtak.konfig.KonfigVerdi;
 import no.nav.vedtak.util.LRUCache;
 
 
@@ -32,25 +36,22 @@ public class AktørTjeneste {
     private LRUCache<AktørId, PersonIdent> cacheAktørIdTilIdent;
     private LRUCache<PersonIdent, AktørId> cacheIdentTilAktørId;
 
-    private PdlKlient pdlKlient;
-
+    private Pdl pdlKlientFOR;
+    private Pdl pdlKlientOMS;
 
     public AktørTjeneste() {
         // for CDI proxy
     }
 
     @Inject
-    public AktørTjeneste(PdlKlient pdlKlient) {
-        this.pdlKlient = pdlKlient;
+    public AktørTjeneste(@KonfigVerdi(value = "pdl.base.url",defaultVerdi = "http://pdl-api.default/graphql") URI endpoint, StsAccessTokenConfig config) {
+        this.pdlKlientFOR = new JerseyPdlKlient(endpoint, config, "FOR");
+        this.pdlKlientOMS = new JerseyPdlKlient(endpoint, config, "OMS");
         this.cacheAktørIdTilIdent = new LRUCache<>(DEFAULT_CACHE_SIZE, DEFAULT_CACHE_TIMEOUT);
         this.cacheIdentTilAktørId = new LRUCache<>(DEFAULT_CACHE_SIZE, DEFAULT_CACHE_TIMEOUT);
     }
 
     public Optional<AktørId> hentAktørForIdent(PersonIdent fnr, YtelseType ytelse) {
-        var tema = ytelse != null ? gjelderTema(ytelse) : null;
-        if (tema == null)
-            throw new IllegalArgumentException("Utviklerfeil mangler fagsystem mot PDL");
-
         var request = new HentIdenterQueryRequest();
         request.setIdent(fnr.getIdent());
         request.setGrupper(List.of(IdentGruppe.AKTORID));
@@ -59,7 +60,7 @@ public class AktørTjeneste {
             .identer(new IdentInformasjonResponseProjection().ident());
 
         try {
-            var identliste = pdlKlient.hentIdenter(request, projection);
+            var identliste = hentIdenterForYtelse(request, projection, ytelse);
             var aktørId = identliste.getIdenter().stream().findFirst().map(IdentInformasjon::getIdent).map(AktørId::new);
             aktørId.ifPresent(a -> cacheIdentTilAktørId.put(fnr, a));
             return aktørId;
@@ -72,10 +73,6 @@ public class AktørTjeneste {
     }
 
     public Optional<PersonIdent> hentIdentForAktør(AktørId aktørId, YtelseType ytelse) {
-        var tema = ytelse != null ? gjelderTema(ytelse) : null;
-        if (tema == null)
-            throw new IllegalArgumentException("Utviklerfeil mangler fagsystem mot PDL");
-
         var fraCache = cacheAktørIdTilIdent.get(aktørId);
         if (fraCache != null) {
             return Optional.of(fraCache);
@@ -90,7 +87,7 @@ public class AktørTjeneste {
         final Identliste identliste;
 
         try {
-            identliste = pdlKlient.hentIdenter(request, projection);
+            identliste = hentIdenterForYtelse(request, projection, ytelse);
             var ident = identliste.getIdenter().stream().findFirst().map(IdentInformasjon::getIdent).map(PersonIdent::new);
             ident.ifPresent(i -> cacheAktørIdTilIdent.put(aktørId, i));
             return ident;
@@ -102,7 +99,11 @@ public class AktørTjeneste {
         }
     }
 
-    private static Tema gjelderTema(YtelseType y) {
-        return Set.of(YtelseType.ENGANGSTØNAD, YtelseType.FORELDREPENGER, YtelseType.SVANGERSKAPSPENGER).contains(y) ? Tema.FOR : Tema.OMS;
+    private Identliste hentIdenterForYtelse(HentIdenterQueryRequest request, IdentlisteResponseProjection projection, YtelseType ytelseType) {
+        return gjelderTemaForeldrepenger(ytelseType) ? pdlKlientFOR.hentIdenter(request, projection) : pdlKlientOMS.hentIdenter(request, projection);
+    }
+
+    private static boolean gjelderTemaForeldrepenger(YtelseType y) {
+        return Set.of(YtelseType.ENGANGSTØNAD, YtelseType.FORELDREPENGER, YtelseType.SVANGERSKAPSPENGER).contains(y);
     }
 }
