a66b17e4c371fc4e6e095d192effb2e07960070f
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2021-04-26 10:57:13 +0200

    Fix: SN inntekter hentes også når oppgitt opptjening er lagret i oppgitt-opptjening-aggregatet (#1352)

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/DefaultIAYRegisterInnhentingTjenesteImpl.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/DefaultIAYRegisterInnhentingTjenesteImpl.java
@@ -1,10 +1,13 @@
 package no.nav.foreldrepenger.abakus.registerdata;
 
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import no.nav.foreldrepenger.abakus.aktor.AktørTjeneste;
 import no.nav.foreldrepenger.abakus.domene.iay.InntektArbeidYtelseGrunnlag;
+import no.nav.foreldrepenger.abakus.domene.iay.søknad.OppgittOpptjeningAggregat;
 import no.nav.foreldrepenger.abakus.iay.InntektArbeidYtelseTjeneste;
 import no.nav.foreldrepenger.abakus.kobling.Kobling;
 import no.nav.foreldrepenger.abakus.kobling.kontroll.YtelseTypeRef;
@@ -38,10 +41,21 @@ public class DefaultIAYRegisterInnhentingTjenesteImpl extends IAYRegisterInnhent
 
     @Override
     public boolean skalInnhenteNæringsInntekterFor(Kobling kobling) {
-        return inntektArbeidYtelseTjeneste.hentGrunnlagFor(kobling.getKoblingReferanse())
+        Optional<InntektArbeidYtelseGrunnlag> grunnlag = inntektArbeidYtelseTjeneste.hentGrunnlagFor(kobling.getKoblingReferanse());
+
+        //FP,SVP,FRISINN bruker ikke aggregat for oppgitt opptjening (støtter kun en pr behandling)
+        boolean harOppgittSNOpptjeningUtenAggregat = grunnlag
             .flatMap(InntektArbeidYtelseGrunnlag::getOppgittOpptjening)
             .map(oppgittOpptjening -> !oppgittOpptjening.getEgenNæring().isEmpty())
             .orElse(false);
+
+        //OMP, PSB bruker aggregat for oppgitt opptjening (støtter mange pr behandling)
+        Optional<OppgittOpptjeningAggregat> aggregat = grunnlag.flatMap(InntektArbeidYtelseGrunnlag::getOppgittOpptjeningAggregat);
+        boolean harOppgittOpptjeningSNMedAggregat = aggregat.isPresent() && aggregat.get().getOppgitteOpptjeninger().stream().anyMatch(oppgittOpptjening -> !oppgittOpptjening.getEgenNæring().isEmpty());
+
+        return harOppgittSNOpptjeningUtenAggregat || harOppgittOpptjeningSNMedAggregat ;
+
+
     }
 
     @Override
