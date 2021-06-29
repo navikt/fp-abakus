876e2ce7b0e39204eeb00048f9e9f5d31af0b08a
Stian Grenborgen <stian.grenborgen@nav.no>
2021-06-29 10:37:25 +0200

    Sletter med bakgrunn i bck-tabeller.

--- migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_004__Rydder-bort-duplikate-referanser-del2.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_004__Rydder-bort-duplikate-referanser-del2.sql
@@ -1,58 +1,38 @@
 DELETE
 FROM iay_aktivitets_avtale
-WHERE yrkesaktivitet_id IN (
-    SELECT ya1.id
-    FROM iay_yrkesaktivitet ya1 INNER JOIN iay_aktoer_arbeid aa1 ON (
-            ya1.aktoer_arbeid_id = aa1.id
-        ) INNER JOIN tmp_feilrettingskandidater tmp1 ON (
-            aa1.inntekt_arbeid_ytelser_id = tmp1.register_id
-        )
+WHERE id IN (
+    SELECT a.id
+    FROM bck_iay_aktivitets_avtale a
 );
 
 DELETE
 FROM iay_permisjon
 WHERE yrkesaktivitet_id IN (
-    SELECT ya1.id
-    FROM iay_yrkesaktivitet ya1 INNER JOIN iay_aktoer_arbeid aa1 ON (
-            ya1.aktoer_arbeid_id = aa1.id
-        ) INNER JOIN tmp_feilrettingskandidater tmp1 ON (
-            aa1.inntekt_arbeid_ytelser_id = tmp1.register_id
-        )
+    SELECT a.id
+    FROM bck_iay_permisjon a
 );
 
 DELETE
 FROM iay_yrkesaktivitet
 WHERE id IN (
     SELECT ya1.id
-    FROM iay_yrkesaktivitet ya1 INNER JOIN iay_aktoer_arbeid aa1 ON (
-            ya1.aktoer_arbeid_id = aa1.id
-        ) INNER JOIN tmp_feilrettingskandidater tmp1 ON (
-            aa1.inntekt_arbeid_ytelser_id = tmp1.register_id
-        )
+    FROM bck_iay_yrkesaktivitet
 );
 
 DELETE
 FROM iay_arbeidsforhold_refer
-WHERE id in (
-    SELECT id
-    FROM iay_arbeidsforhold_refer r INNER JOIN tmp_feilrettingskandidater tmp1 ON (
-                r.informasjon_id = tmp1.informasjon_id
-            AND r.arbeidsgiver_orgnr = tmp1.arbeidsgiver_orgnr
-            AND (
-                                tmp1.antall_im1 = 0 AND r.intern_referanse= tmp1.arbeidsforhold_intern_id1
-                        OR
-                                tmp1.antall_im2 = 0 AND r.intern_referanse = tmp1.arbeidsforhold_intern_id2
-                    )
-        )
+WHERE id IN (
+    SELECT a.id
+    FROM bck_iay_arbeidsforhold_refer a
 );
 
 UPDATE prosess_task pt
 SET status = 'KLAR', feilede_forsoek=0
 FROM (
-         SELECT max(id) as id, tmp1.kobling_id
-         FROM prosess_task p INNER JOIN tmp_feilrettingskandidater tmp1 ON (
-                                                                                   tmp1.kobling_id = CAST(SUBSTRING(p.task_parametere, 'behandlingId=([^[:space:]]*)') AS BIGINT)
-                                                                               ) AND p.task_type = 'registerdata.innhent'
-         GROUP BY tmp1.kobling_id
-     ) AS subquery
+   SELECT max(id) as id, tmp1.kobling_id
+   FROM prosess_task p INNER JOIN tmp_feilrettingskandidater tmp1 ON (
+      tmp1.kobling_id = CAST(SUBSTRING(p.task_parametere, 'behandlingId=([^[:space:]]*)') AS BIGINT)
+   ) AND p.task_type = 'registerdata.innhent'
+   GROUP BY tmp1.kobling_id
+) AS subquery
 WHERE pt.id=subquery.id;
