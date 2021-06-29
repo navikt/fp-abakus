0ab7ef772c15ecbc21d83d98c94ceba09a3080d8
Stian Grenborgen <stian.grenborgen@nav.no>
2021-06-29 11:34:18 +0200

    Feature/rydder etter duplikate areidsforhold PSB (#1472)
    * Rydder opp etter feil som ga duplikate arbeidsforholdsId osv. Fiksen er kun for ytelse PSB.

---
+++ migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_004__Rydder-bort-duplikate-referanser-del2.sql
@@ -0,0 +1,38 @@
+DELETE
+FROM iay_aktivitets_avtale
+WHERE id IN (
+    SELECT a.id
+    FROM bck_psb_iay_aktivitets_avtale a
+);
+
+DELETE
+FROM iay_permisjon
+WHERE id IN (
+    SELECT a.id
+    FROM bck_psb_iay_permisjon a
+);
+
+DELETE
+FROM iay_yrkesaktivitet
+WHERE id IN (
+    SELECT a.id
+    FROM bck_psb_iay_yrkesaktivitet a
+);
+
+DELETE
+FROM iay_arbeidsforhold_refer
+WHERE id IN (
+    SELECT a.id
+    FROM bck_psb_iay_arbeidsforhold_refer a
+);
+
+UPDATE prosess_task pt
+SET status = 'KLAR', feilede_forsoek=0
+FROM (
+   SELECT max(id) as id, tmp1.kobling_id
+   FROM prosess_task p INNER JOIN tmp_psb_feilrettingskandidater tmp1 ON (
+      tmp1.kobling_id = CAST(SUBSTRING(p.task_parametere, 'behandlingId=([^[:space:]]*)') AS BIGINT)
+   ) AND p.task_type = 'registerdata.innhent'
+   GROUP BY tmp1.kobling_id
+) AS subquery
+WHERE pt.id=subquery.id;
