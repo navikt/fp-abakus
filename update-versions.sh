ca117f0f9a1e87bdeffde7ef59a2b59e685d9944
Michal J. Sladek <michal.sladek@nav.no>
2021-11-23 08:25:24 +0100

    Fikset lokal docker oppsett.

--- update-versions.sh
+++ update-versions.sh
@@ -2,6 +2,6 @@
 
 echo VTP_IMAGE=ghcr.io/navikt/vtp > .env
 echo POSTGRES_IMAGE=postgres:12 >> .env
-echo AZURE_MOCK_IMAGE=docker.pkg.github.com/navikt/azure-mock/azure-mock:latest >> .env
+echo AZURE_MOCK_IMAGE=ghcr.io/navikt/azure-mock/azure-mock:latest >> .env
 
 echo ".env fil opprettet - Klart for docker-compose up"
