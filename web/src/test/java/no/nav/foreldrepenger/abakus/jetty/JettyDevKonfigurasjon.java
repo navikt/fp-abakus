787e89ba9de864a209cf74359524513417a32d33
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-01-18 11:09:06 +0100

    Jetty 10.0.7, rydding og oppdatert felles/task (#1736)
    * Jetty 10.0.7, rydding og oppdatert felles/task
    
    * Flyway er ute av test-utilities
    
    * Enda en flyway dependency for test
    
    * Enda mer deps

--- web/src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevKonfigurasjon.java
+++ web/src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevKonfigurasjon.java
@@ -1,16 +1,11 @@
 package no.nav.foreldrepenger.abakus.jetty;
 
 public class JettyDevKonfigurasjon extends JettyWebKonfigurasjon {
-    private static final int SSL_SERVER_PORT = 8043;
     private static final int DEV_SERVER_PORT = 8015;
 
     public JettyDevKonfigurasjon() {
         super(DEV_SERVER_PORT);
     }
 
-    @Override
-    public int getSslPort() {
-        return SSL_SERVER_PORT;
-    }
 
 }
