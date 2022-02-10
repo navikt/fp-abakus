0103b73fe6e046bc08921fb7f15383f2895aa614
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-02-10 13:08:43 +0100

    Fjerne sam, oppdatere deps

--- web/src/main/java/no/nav/foreldrepenger/abakus/jetty/JettyWebKonfigurasjon.java
+++ web/src/main/java/no/nav/foreldrepenger/abakus/jetty/JettyWebKonfigurasjon.java
@@ -1,14 +1,18 @@
 package no.nav.foreldrepenger.abakus.jetty;
 
+import no.nav.vedtak.sikkerhet.ContextPathHolder;
+
 public class JettyWebKonfigurasjon implements AppKonfigurasjon {
     private static final String CONTEXT_PATH = "/fpabakus";
 
     private Integer serverPort;
 
     public JettyWebKonfigurasjon() {
+        ContextPathHolder.instance(CONTEXT_PATH);
     }
 
     public JettyWebKonfigurasjon(int serverPort) {
+        this();
         this.serverPort = serverPort;
     }
 
