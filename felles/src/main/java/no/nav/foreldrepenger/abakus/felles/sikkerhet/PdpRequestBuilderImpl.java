048c644359293d8563db572b090616873f4be8d4
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-09-05 22:53:58 +0200

    Forenkling av abac, contract-fase

--- felles/src/main/java/no/nav/foreldrepenger/abakus/felles/sikkerhet/PdpRequestBuilderImpl.java
+++ felles/src/main/java/no/nav/foreldrepenger/abakus/felles/sikkerhet/PdpRequestBuilderImpl.java
@@ -2,15 +2,12 @@ package no.nav.foreldrepenger.abakus.felles.sikkerhet;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import no.nav.vedtak.sikkerhet.abac.AbacAttributtSamling;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
-import no.nav.vedtak.sikkerhet.abac.PdpKlient;
-import no.nav.vedtak.sikkerhet.abac.PdpRequest;
 import no.nav.vedtak.sikkerhet.abac.PdpRequestBuilder;
 import no.nav.vedtak.sikkerhet.abac.StandardAbacAttributtType;
 import no.nav.vedtak.sikkerhet.abac.pdp.AppRessursData;
-import no.nav.vedtak.sikkerhet.abac.pdp.BehandlingStatus;
-import no.nav.vedtak.sikkerhet.abac.pdp.FagsakStatus;
+import no.nav.vedtak.sikkerhet.abac.pipdata.PipBehandlingStatus;
+import no.nav.vedtak.sikkerhet.abac.pipdata.PipFagsakStatus;
 
 /**
  * Implementasjon av PDP request for denne applikasjonen.
@@ -21,28 +18,8 @@ public class PdpRequestBuilderImpl implements PdpRequestBuilder {
     public static final String ABAC_DOMAIN = "duplo";
 
     @Override
-    public PdpRequest lagPdpRequest(AbacAttributtSamling attributter) {
-        PdpRequest pdpRequest = new PdpRequest();
-        pdpRequest.put(PdpKlient.ENVIRONMENT_AUTH_TOKEN, attributter.getIdToken());
-        pdpRequest.put(AbacAttributter.XACML_1_0_ACTION_ACTION_ID, attributter.getActionType().getEksternKode());
-        pdpRequest.put(AbacAttributter.RESOURCE_FELLES_DOMENE, ABAC_DOMAIN);
-        pdpRequest.put(AbacAttributter.RESOURCE_FELLES_RESOURCE_TYPE, AbacResourceMapper.mapResource(attributter.getResource()));
-        pdpRequest.put(AbacAttributter.RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE, attributter.getVerdier(StandardAbacAttributtType.AKTØR_ID));
-        pdpRequest.put(AbacAttributter.RESOURCE_FELLES_PERSON_FNR, attributter.getVerdier(StandardAbacAttributtType.FNR));
-        // TODO: Hente fra pip-tjenesten?
-        pdpRequest.put(AbacAttributter.RESOURCE_FORELDREPENGER_SAK_BEHANDLINGSSTATUS, AbacBehandlingStatus.UTREDES.getEksternKode());
-        pdpRequest.put(AbacAttributter.RESOURCE_FORELDREPENGER_SAK_SAKSSTATUS, AbacFagsakStatus.UNDER_BEHANDLING.getEksternKode());
-        return pdpRequest;
-    }
-
-    @Override
     public String abacDomene() {
         return ABAC_DOMAIN;
-    }
-
-    @Override
-    public boolean nyttAbacGrensesnitt() {
-        return true;
     }
 
     @Override
@@ -51,8 +28,8 @@ public class PdpRequestBuilderImpl implements PdpRequestBuilder {
             .leggTilAktørIdSet(dataAttributter.getVerdier(StandardAbacAttributtType.AKTØR_ID))
             .leggTilFødselsnumre(dataAttributter.getVerdier(StandardAbacAttributtType.FNR))
             // TODO: Hente fra pip-tjenesten? arv fra tidligere... men nå er 2 pips aktuelle ....
-            .medFagsakStatus(FagsakStatus.UNDER_BEHANDLING)
-            .medBehandlingStatus(BehandlingStatus.UTREDES)
+            .medFagsakStatus(PipFagsakStatus.UNDER_BEHANDLING)
+            .medBehandlingStatus(PipBehandlingStatus.UTREDES)
             .build();
     }
 }
