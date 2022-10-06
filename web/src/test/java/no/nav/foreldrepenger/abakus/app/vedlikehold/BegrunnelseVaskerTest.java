be135a4cdf190f52de9ea5fd7159f8e2334804d0
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-10-06 14:10:11 +0200

    Vasker også for nbsp

---
+++ web/src/test/java/no/nav/foreldrepenger/abakus/app/vedlikehold/BegrunnelseVaskerTest.java
@@ -0,0 +1,25 @@
+package no.nav.foreldrepenger.abakus.app.vedlikehold;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class BegrunnelseVaskerTest {
+
+    @Test
+    void skal_vaske_begrunnelse() {
+
+        var uvasket = "Gått fra \n" +
+            "usammenhengende vikariater med varierende arbeidsomfang,til fast kontrakt med mer stabil og \n" +
+            "høyere inntekt.";
+
+
+        var forventet = "Gått fra " +
+            "usammenhengende vikariater med varierende arbeidsomfang,til fast kontrakt med mer stabil og " +
+            "høyere inntekt.";
+
+        var vasket = BegrunnelseVasker.vask(uvasket);
+
+        assertThat(vasket).isEqualTo(forventet);
+    }
+}
