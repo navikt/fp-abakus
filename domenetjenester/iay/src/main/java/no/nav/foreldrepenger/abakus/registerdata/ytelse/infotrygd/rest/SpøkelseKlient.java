3fec426e65da99d8d683f898bceaf6b10c101ef4
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-02-22 12:55:34 +0100

    Spøkelse gcp prod

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/rest/SpøkelseKlient.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/rest/SpøkelseKlient.java
@@ -7,7 +7,7 @@ import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 import no.nav.vedtak.felles.integrasjon.spokelse.AbstractSpøkelseKlient;
 
 @Dependent
-@RestClientConfig(tokenConfig = TokenFlow.AZUREAD_CC, endpointProperty = "SPOKELSE_GRUNNLAG_URL", endpointDefault = "http://spokelse.tbd/grunnlag", scopesProperty = "SPOKELSE_GRUNNLAG_SCOPES", scopesDefault = "api://prod-fss.tbd.spokelse/.default")
+@RestClientConfig(tokenConfig = TokenFlow.AZUREAD_CC, endpointProperty = "spokelse.grunnlag.url", endpointDefault = "http://spokelse.tbd.svc.cluster.local/grunnlag", scopesProperty = "spokelse.grunnlag.scopes", scopesDefault = "api://prod-gcp.tbd.spokelse/.default")
 public class SpøkelseKlient extends AbstractSpøkelseKlient {
 
     public SpøkelseKlient() {
