30d7ca2669ab9db1bd4b177021af6ed983ae3ee2
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-03-06 20:19:27 +0100

    Prosesstask bruker azure når adaptive

--- domenetjenester/kobling/src/main/java/no/nav/foreldrepenger/abakus/kobling/TaskConstants.java
+++ domenetjenester/kobling/src/main/java/no/nav/foreldrepenger/abakus/kobling/TaskConstants.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.abakus.kobling;
 public final class TaskConstants {
     public static final String KOBLING_ID = "behandlingId";
     public static final String CALLBACK_URL = "callbackUrl";
+    public static final String CALLBACK_SCOPE = "callbackScope";
     private TaskConstants() {
     }
 }
