600fa58180269f4552bafe3a226dff7d063ed687
Michal J. Sladek <michal.sladek@nav.no>
2023-06-01 13:01:25 +0200

    Bumper lokalt oppsett til postgres 15

--- lokalutvikling/postgres-init/fpabakus.sql
+++ lokalutvikling/postgres-init/fpabakus.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE fpabakus_unit;
 CREATE USER fpabakus_unit WITH PASSWORD 'fpabakus_unit';
-GRANT ALL PRIVILEGES ON DATABASE fpabakus_unit TO fpabakus_unit;
+GRANT ALL ON DATABASE fpabakus_unit TO fpabakus_unit;
+ALTER DATABASE fpabakus_unit SET timezone TO 'Europe/Oslo';
+ALTER DATABASE fpabakus_unit OWNER TO fpabakus_unit;
 
 CREATE DATABASE fpabakus;
 CREATE USER fpabakus WITH PASSWORD 'fpabakus';
-GRANT ALL PRIVILEGES ON DATABASE fpabakus TO fpabakus;
+GRANT ALL ON DATABASE fpabakus TO fpabakus;
+ALTER DATABASE fpabakus SET timezone TO 'Europe/Oslo';
+ALTER DATABASE fpabakus OWNER TO fpabakus;
