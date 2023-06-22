4fe68de0dc6c460af1490a2bcbd39c194e096dc6
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-06-22 11:23:56 +0200

    Flytter utvidelse av opptjeningsperioden så den treffer begge kall til sigrun consumer

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/klient/SigrunConsumerImpl.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/klient/SigrunConsumerImpl.java
@@ -13,9 +13,6 @@ import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import no.nav.foreldrepenger.abakus.felles.jpa.IntervallEntitet;
 import no.nav.foreldrepenger.abakus.registerdata.inntekt.sigrun.klient.summertskattegrunnlag.SSGResponse;
 import no.nav.foreldrepenger.abakus.registerdata.inntekt.sigrun.klient.summertskattegrunnlag.SigrunSummertSkattegrunnlagResponse;
@@ -24,8 +21,6 @@ import no.nav.vedtak.felles.integrasjon.rest.RestClient;
 
 @ApplicationScoped
 public class SigrunConsumerImpl implements SigrunConsumer {
-
-    private static final Logger logger = LoggerFactory.getLogger(SigrunConsumerImpl.class);
 
     private static final String TEKNISK_NAVN = "skatteoppgjoersdato";
 
@@ -59,12 +54,20 @@ public class SigrunConsumerImpl implements SigrunConsumer {
         return new SigrunSummertSkattegrunnlagResponse(summertskattegrunnlagMap);
     }
 
+    @Override
+    public boolean erÅretFerdiglignet(Long aktørId, Year år) {
+        if (isProd && Year.now().minusYears(1).equals(år) && MonthDay.now().isBefore(TIDLIGSTE_SJEKK_FJOR)) {
+            return false;
+        }
+        return client.hentBeregnetSkattForAktørOgÅr(aktørId, år.toString()).stream().anyMatch(l -> l.tekniskNavn().equals(TEKNISK_NAVN));
+    }
+
     private List<Year> ferdiglignedeBeregnetSkattÅr(Long aktørId, IntervallEntitet opplysningsperiode) {
         if (opplysningsperiode != null) {
             return beregnetSkattÅrslisteFraOpplysningsperiode(opplysningsperiode);
         } else {
             Year iFjor = Year.now().minusYears(1L);
-            if (iFjorErFerdiglignetBeregnet(aktørId, iFjor)) {
+            if (erÅretFerdiglignet(aktørId, iFjor)) {
                 return asList(iFjor, iFjor.minusYears(1L), iFjor.minusYears(2L));
             } else {
                 Year iForifjor = iFjor.minusYears(1L);
@@ -88,8 +91,7 @@ public class SigrunConsumerImpl implements SigrunConsumer {
     List<Year> hentÅrsListeForSummertskattegrunnlag(Long aktørId, IntervallEntitet opplysningsperiode) {
         Year iFjor = Year.now().minusYears(1L);
         if (opplysningsperiode != null) {
-            var justertOpplysningsperiode = justerOpplysningsperiodeNårSisteÅrIkkeErFerdiglignet(aktørId, opplysningsperiode);
-            return summertSkattegrunnlagÅrslisteFraOpplysningsperiode(justertOpplysningsperiode);
+            return summertSkattegrunnlagÅrslisteFraOpplysningsperiode(opplysningsperiode);
         } else {
             //filteret(SummertSkattegrunnlagForeldrepenger) i Sigrun er ikke impl. tidligere enn 2018
             if (iFjor.equals(Year.of(2018))) {
@@ -99,25 +101,6 @@ public class SigrunConsumerImpl implements SigrunConsumer {
             }
             return ferdiglignedeBeregnetSkattÅr(aktørId, opplysningsperiode);
         }
-    }
-
-    private IntervallEntitet justerOpplysningsperiodeNårSisteÅrIkkeErFerdiglignet(Long aktørId, IntervallEntitet opplysningsperiode) {
-        // justerer slik at vi henter ett å eldre data når siste år som etterspørs ikke er ferdiglignet enda
-        int fomÅr = opplysningsperiode.getFomDato().getYear();
-        int tomÅr = opplysningsperiode.getTomDato().getYear();
-        Year iFjor = Year.now().minusYears(1);
-        logger.info("Opprinnelig opplysningsperiode er fom {} tom {}", fomÅr, tomÅr);
-        if (opplysningsperiode.getTomDato().getYear() == iFjor.getValue()) {
-            boolean fjordårdetFerdiglignet = iFjorErFerdiglignetBeregnet(aktørId, iFjor);
-            logger.info("Ferdiglignet {}", fjordårdetFerdiglignet);
-            if (!fjordårdetFerdiglignet && tomÅr - fomÅr < 3) { //dataminimering, ikke behov for data ut over 3 hele år før første stp
-                logger.info("Utvider opplysningsperioden med ett år pga ikke-ferdiglignet år");
-                fomÅr--;
-            }
-        }
-        LocalDate fom = LocalDate.of(fomÅr, 1, 1);
-        LocalDate tom = LocalDate.of(tomÅr, 12, 31);
-        return IntervallEntitet.fraOgMedTilOgMed(fom, tom);
     }
 
     private ArrayList<Year> summertSkattegrunnlagÅrslisteFraOpplysningsperiode(IntervallEntitet opplysningsperiode) {
@@ -133,13 +116,6 @@ public class SigrunConsumerImpl implements SigrunConsumer {
             år++;
         }
         return årsListe;
-    }
-
-    private boolean iFjorErFerdiglignetBeregnet(Long aktørId, Year iFjor) {
-        if (isProd && Year.now().minusYears(1).equals(iFjor) && MonthDay.now().isBefore(TIDLIGSTE_SJEKK_FJOR)) {
-            return false;
-        }
-        return client.hentBeregnetSkattForAktørOgÅr(aktørId, iFjor.toString()).stream().anyMatch(l -> l.tekniskNavn().equals(TEKNISK_NAVN));
     }
 
 
