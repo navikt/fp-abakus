e529eaf8c5cc8f98f4bd0a832d56fc26343741a6
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-06-23 12:40:31 +0200

    Legger koblingId i MDC for KoblingTask

--- domenetjenester/kobling/src/main/java/no/nav/foreldrepenger/abakus/kobling/KoblingTask.java
+++ domenetjenester/kobling/src/main/java/no/nav/foreldrepenger/abakus/kobling/KoblingTask.java
@@ -3,8 +3,11 @@ package no.nav.foreldrepenger.abakus.kobling;
 import no.nav.foreldrepenger.abakus.kobling.repository.LåsRepository;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.log.mdc.MdcExtendedLogContext;
 
 public abstract class KoblingTask implements ProsessTaskHandler {
+
+    private static final MdcExtendedLogContext LOG_CONTEXT = MdcExtendedLogContext.getContext("prosess");
 
     private LåsRepository låsRepository;
 
@@ -19,6 +22,7 @@ public abstract class KoblingTask implements ProsessTaskHandler {
     public void doTask(ProsessTaskData prosessTaskData) {
         String nyKoblingId = prosessTaskData.getPropertyValue(TaskConstants.KOBLING_ID);
         Long koblingId = nyKoblingId != null ? Long.valueOf(nyKoblingId) : Long.valueOf(prosessTaskData.getBehandlingId());
+        LOG_CONTEXT.add("koblingId", koblingId);
 
         KoblingLås koblingLås = låsRepository.taLås(koblingId);
 
