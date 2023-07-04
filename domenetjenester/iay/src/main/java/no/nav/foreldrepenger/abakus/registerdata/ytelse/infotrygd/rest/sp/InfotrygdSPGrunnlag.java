f4d9138c313f7c453e422e5f07a23f8c7dd9f6fd
Michal J. Sladek <michal.sladek@nav.no>
2023-07-04 20:58:32 +0200

    Fikser token flow

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/rest/sp/InfotrygdSPGrunnlag.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/rest/sp/InfotrygdSPGrunnlag.java
@@ -8,7 +8,7 @@ import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
 @SP
 @Dependent
-@RestClientConfig(tokenConfig = TokenFlow.STS_CC, endpointProperty = "fpabakus.it.sp.grunnlag.url",
+@RestClientConfig(tokenConfig = TokenFlow.AZUREAD_CC, endpointProperty = "fpabakus.it.sp.grunnlag.url",
     endpointDefault = "http://infotrygd-sykepenger-fp.infotrygd/grunnlag",
     scopesProperty = "fpabakus.it.sp.scopes", scopesDefault = "api://prod-fss.infotrygd.infotrygd-sykepenger-fp/.default")
 public class InfotrygdSPGrunnlag extends AbstractInfotrygdGrunnlag {
