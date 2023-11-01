dcf2fd6f282fcfc060ff4e1a5e8bbd77ca9c274d
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-11-01 23:09:20 +0100

    Tilpass til faktisk svar fra Sigrun v/stub

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/klient/pgifolketrygden/SigrunPgiFolketrygdenMapper.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/klient/pgifolketrygden/SigrunPgiFolketrygdenMapper.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import no.nav.abakus.iaygrunnlag.kodeverk.InntektspostType;
@@ -18,26 +17,27 @@ public final class SigrunPgiFolketrygdenMapper {
     public static Map<IntervallEntitet, Map<InntektspostType, BigDecimal>> mapFraSigrunTilIntern(SigrunPgiFolketrygdenResponse response) {
         Map<IntervallEntitet, Map<InntektspostType, BigDecimal>> resultat = new LinkedHashMap<>();
         response.pgiFolketrygdenMap().values().stream()
-            .filter(Optional::isPresent)
-            .map(pgiFolketrygdenResponse -> mapPgiFolketrygden(pgiFolketrygdenResponse.get()))
+            .map(SigrunPgiFolketrygdenMapper::mapPgiFolketrygden)
             .forEach(resultat::putAll);
         return resultat;
     }
 
-    public static Map<IntervallEntitet, Map<InntektspostType, BigDecimal>> mapPgiFolketrygden(PgiFolketrygdenResponse response) {
-        if (response == null) {
+    public static Map<IntervallEntitet, Map<InntektspostType, BigDecimal>> mapPgiFolketrygden(List<PgiFolketrygdenResponse> response) {
+        if (response == null || response.isEmpty()) {
             return Map.of();
         }
         var inntektBeløp = new ArrayList<InntektBeløp>();
-        for (PgiFolketrygdenResponse.Pgi pgi : response.pensjonsgivendeInntekt()) {
-            leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvLoennsinntekt(), InntektspostType.LØNN, inntektBeløp);
-            leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel(), InntektspostType.LØNN, inntektBeløp);
-            leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvNaeringsinntekt(), InntektspostType.SELVSTENDIG_NÆRINGSDRIVENDE, inntektBeløp);
-            leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage(), InntektspostType.NÆRING_FISKE_FANGST_FAMBARNEHAGE, inntektBeløp);
+        for (var responselement : response) {
+            for (var pgi : responselement.pensjonsgivendeInntekt()) {
+                leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvLoennsinntekt(), InntektspostType.LØNN, inntektBeløp);
+                leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel(), InntektspostType.LØNN, inntektBeløp);
+                leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvNaeringsinntekt(), InntektspostType.SELVSTENDIG_NÆRINGSDRIVENDE, inntektBeløp);
+                leggTilHvisVerdi(pgi.pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage(), InntektspostType.NÆRING_FISKE_FANGST_FAMBARNEHAGE, inntektBeløp);
+            }
         }
         var inntektBeløpMap = inntektBeløp.stream()
             .collect(Collectors.groupingBy(InntektBeløp::inntektspostType, Collectors.reducing(BigDecimal.ZERO, InntektBeløp::beløp, BigDecimal::add)));
-        var år = Year.of(response.inntektsaar());
+        var år = Year.of(response.get(0).inntektsaar());
         var førsteDagIÅret = LocalDate.now().with(år).withDayOfYear(1);
         var sisteDagIÅret = LocalDate.now().with(år).withDayOfYear(år.length());
         var intervall = IntervallEntitet.fraOgMedTilOgMed(førsteDagIÅret, sisteDagIÅret);
