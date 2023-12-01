130512c6968fd2f5ef8938dcb7653bc0a44ec2ab
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-12-01 08:46:20 +0100

    Postgres 16 + fp-bom 2.1.0 (#2471)

--- lokalutvikling/update-versions.sh
+++ lokalutvikling/update-versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo VTP_IMAGE=ghcr.io/navikt/vtp > .env
-echo POSTGRES_IMAGE=postgres:15 >> .env
+echo POSTGRES_IMAGE=postgres:16 >> .env
 echo AZURE_MOCK_IMAGE=ghcr.io/navikt/azure-mock/azure-mock:latest >> .env
 
 echo ".env fil opprettet - Klart for docker-compose up"
