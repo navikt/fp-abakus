8680b63bd3175a42ed6f54a3623abc073e605b4d
Michal J. Sladek <michal.sladek@nav.no>
2024-01-03 09:54:20 +0100

    Rebase fra master - fikset bygg

--- kodeverk/src/main/java/no/nav/abakus/iaygrunnlag/kodeverk/ArbeidsforholdHandlingType.java
+++ kodeverk/src/main/java/no/nav/abakus/iaygrunnlag/kodeverk/ArbeidsforholdHandlingType.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
