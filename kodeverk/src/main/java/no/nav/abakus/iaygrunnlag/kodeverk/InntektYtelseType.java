ce6373d6bfcde3fe21c8c381749a713b6dd56c9d
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-01-15 18:12:28 +0100

    Ekstra mapping

--- kodeverk/src/main/java/no/nav/abakus/iaygrunnlag/kodeverk/InntektYtelseType.java
+++ kodeverk/src/main/java/no/nav/abakus/iaygrunnlag/kodeverk/InntektYtelseType.java
@@ -59,7 +59,7 @@ public enum InntektYtelseType implements Kodeverdi {
     ANNET("Annet", Kategori.NÆRING, YtelseType.UDEFINERT, "annet"),
     VEDERLAG("Vederlag", Kategori.NÆRING, YtelseType.UDEFINERT, List.of("vederlag", "vederlagDagmammaIEgetHjem")),
     LOTT_KUN_TRYGDEAVGIFT("Lott kun trygdeavgift", Kategori.NÆRING, YtelseType.UDEFINERT, "lottKunTrygdeavgift"),
-    KOMPENSASJON_FOR_TAPT_PERSONINNTEKT("Kompensasjon for tapt personinntekt", Kategori.NÆRING, YtelseType.UDEFINERT, "kompensasjonForTaptPersoninntekt")
+    KOMPENSASJON_FOR_TAPT_PERSONINNTEKT("Kompensasjon for tapt personinntekt", Kategori.NÆRING, YtelseType.FRISINN, "kompensasjonForTaptPersoninntekt")
     ;
 
     public static final String KODEVERK = "INNTEKT_YTELSE_TYPE";
