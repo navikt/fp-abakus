0a8c08e650289450e99bf9463cb0437d3d8134f8
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-02-29 16:36:06 +0100

    Bedre sammenligning sykepenger (#2519)
    * Bedre sammenligning sykepenger
    
    * Fikse flere avvik
    
    * Mer nullsafe

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/rest/felles/InfotrygdGrunnlagAggregator.java
+++ domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/ytelse/infotrygd/rest/felles/InfotrygdGrunnlagAggregator.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +18,16 @@ import no.nav.foreldrepenger.abakus.registerdata.ytelse.infotrygd.rest.sp.SP;
 import no.nav.foreldrepenger.abakus.registerdata.ytelse.infotrygd.rest.sp.TSP;
 import no.nav.foreldrepenger.abakus.registerdata.ytelse.infotrygd.rest.sp.TestInfotrygdSPGrunnlag;
 import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.InfotrygdGrunnlag;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Arbeidsforhold;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Arbeidskategori;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Behandlingstema;
 import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Grunnlag;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Inntektsperiode;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Orgnummer;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Periode;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Status;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Tema;
+import no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons.Vedtak;
 
 @ApplicationScoped
 public class InfotrygdGrunnlagAggregator {
@@ -50,7 +60,7 @@ public class InfotrygdGrunnlagAggregator {
         try {
             var gs = sykepenger.hentGrunnlagFailSoft(fnr, fom, tom);
             var gt = testSykepenger.hentGrunnlagFailSoft(fnr, fom, tom);
-            if (gs.size() == gt.size() && gs.containsAll(gt)) {
+            if (erLike(gs, gt)) {
                 LOG.info("ABAKUS-SYKEPENGER like svar");
             } else {
                 LOG.info("ABAKUS-SYKEPENGER ulike svar gamle {} nye {}", gs, gt);
@@ -63,6 +73,52 @@ public class InfotrygdGrunnlagAggregator {
     @Override
     public String toString() {
         return getClass().getSimpleName() + "[tjenester=" + tjenester + "]";
+    }
+
+    private boolean erLike(List<Grunnlag> g1, List<Grunnlag> g2) {
+        var g1s = g1.stream().map(g -> new SammenG(g)).toList();
+        var g2s = g2.stream().map(g -> new SammenG(g)).toList();
+
+        return g1s.size() == g2s.size() && g1s.containsAll(g2s);
+    }
+
+    private record SammenG(Status status, Tema tema, Arbeidskategori kategori, List<Arbeidsforhold> arbeidsforhold, Periode periode, Behandlingstema behandlingstema, LocalDate identdato, LocalDate opphørFom, List<Vedtak> vedtak) {
+        public SammenG(Grunnlag g) {
+            this(g.status(), g.tema(), g.kategori(), g.arbeidsforhold(), g.periode(), g.behandlingstema(), g.identdato(), g.opphørFom(), g.vedtak());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            SammenG sammenG = (SammenG) o;
+            return Objects.equals(status, sammenG.status) && Objects.equals(tema, sammenG.tema) && Objects.equals(kategori, sammenG.kategori)
+                &&  Objects.equals(periode, sammenG.periode) && Objects.equals(
+                behandlingstema, sammenG.behandlingstema) && Objects.equals(identdato, sammenG.identdato) && Objects.equals(opphørFom,
+                sammenG.opphørFom) && vedtak.size() == sammenG.vedtak.size() && vedtak.containsAll(sammenG.vedtak) &&
+                arbeidsforhold.size() == sammenG.arbeidsforhold.size() && arbeidsforhold.containsAll(sammenG.arbeidsforhold);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(status, tema, kategori, arbeidsforhold, periode, behandlingstema, identdato, opphørFom, vedtak);
+        }
+    }
+
+    private record SammenV(Periode periode, int utbetalingsgrad, String arbeidsgiverOrgnr, Boolean erRefusjon, Integer dagsats) {
+
+        public SammenV(Vedtak v) {
+            this(v.periode(), v.utbetalingsgrad(), v.arbeidsgiverOrgnr() == null || Objects.equals(v.arbeidsgiverOrgnr(), "0") ? null : v.arbeidsgiverOrgnr()  , v.erRefusjon(), v.dagsats())
+        }
+    }
+
+    private record SammenA(Orgnummer orgnr, Integer inntekt, Inntektsperiode inntektsperiode, Boolean refusjon, LocalDate refusjonTom) {
+
+        public SammenA(Arbeidsforhold a) {
+            this(a.orgnr() == null || a.orgnr().orgnr() == null || Objects.equals(a.orgnr().orgnr(), "0") ? null : a.orgnr(), a.inntekt(), a.inntektsperiode(), a.refusjon(), a.refusjonTom());
+        }
     }
 
 }
