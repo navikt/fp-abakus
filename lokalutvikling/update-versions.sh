c827e50f8d5e2c06159520057ff9ee5ef3ba24dc
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-03-18 16:53:53 +0100

    ABAC foreldrepenger + fix bygg (#2541)
    Fikser bygg.

--- lokalutvikling/update-versions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo VTP_IMAGE=europe-north1-docker.pkg.dev/nais-management-233d/teamforeldrepenger/navikt/vtp > .env
-echo POSTGRES_IMAGE=postgres:16 >> .env
-
-echo ".env fil opprettet - Klart for docker-compose up"
