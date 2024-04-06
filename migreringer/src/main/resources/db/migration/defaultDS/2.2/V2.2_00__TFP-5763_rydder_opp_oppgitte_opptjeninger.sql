424b9f2eb8d30f436efb265489a254c7b8d4163b
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-04-06 16:37:48 +0200

    TFP-5763: Rydder opp etter oppgitte opptjeninger. (#2558)

---
+++ migreringer/src/main/resources/db/migration/defaultDS/2.2/V2.2_00__TFP-5763_rydder_opp_oppgitte_opptjeninger.sql
@@ -0,0 +1,4 @@
+alter table gr_arbeid_inntekt drop column oppgitte_opptjeninger_id;
+alter table iay_oppgitt_opptjening drop column oppgitte_opptjeninger_id;
+drop table iay_oppgitte_opptjeninger;
+drop sequence seq_iay_oppgitte_opptjeninger;
