bc0f96514ffc88f9bb0dec5be344755c67fc7c33
Michal J. Sladek <michal.sladek@nav.no>
2024-11-13 16:51:38 +0100

    Test jeckson + testcontainer

--- lokalutvikling/postgres-init/fpabakus.sql
+++ lokalutvikling/postgres-init/fpabakus.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE fpabakus_unit;
-CREATE USER fpabakus_unit WITH PASSWORD 'fpabakus_unit';
-GRANT ALL ON DATABASE fpabakus_unit TO fpabakus_unit;
-ALTER DATABASE fpabakus_unit SET timezone TO 'Europe/Oslo';
-ALTER DATABASE fpabakus_unit OWNER TO fpabakus_unit;
-
 CREATE DATABASE fpabakus;
 CREATE USER fpabakus WITH PASSWORD 'fpabakus';
 GRANT ALL ON DATABASE fpabakus TO fpabakus;
--- migreringer/src/test/java/no/nav/foreldrepenger/abakus/dbstoette/JpaExtension.java
+++ migreringer/src/test/java/no/nav/foreldrepenger/abakus/dbstoette/JpaExtension.java
@@ -1,24 +1,21 @@
 package no.nav.foreldrepenger.abakus.dbstoette;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareExtension;
 
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
 public class JpaExtension extends EntityManagerAwareExtension {
 
-    private static final Logger LOG = LoggerFactory.getLogger(JpaExtension.class);
-    private static final boolean isNotRunningUnderMaven = Environment.current().getProperty("maven.cmd.line.args") == null;
+    private static final String TEST_DB_CONTAINER = Environment.current().getProperty("testcontainer.test.db", String.class, "postgres:17-alpine");
+    private static final PostgreSQLContainer TEST_DATABASE;
 
     static {
-        if (isNotRunningUnderMaven) {
-            LOG.info("Kjører IKKE under maven");
-            // prøver alltid migrering hvis endring, ellers funker det dårlig i IDE.
-            Databaseskjemainitialisering.migrerUnittestSkjemaer();
-        }
-        Databaseskjemainitialisering.initUnitTestDataSource();
+        TEST_DATABASE = new PostgreSQLContainer<>(DockerImageName.parse(TEST_DB_CONTAINER)).withReuse(true);
+        TEST_DATABASE.start();
+        TestDatabaseInit.settOppDatasourceOgMigrer(TEST_DATABASE.getJdbcUrl(), TEST_DATABASE.getUsername(), TEST_DATABASE.getPassword());
     }
 
 }
