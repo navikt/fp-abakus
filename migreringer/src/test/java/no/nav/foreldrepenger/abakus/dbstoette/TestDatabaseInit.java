c0aadb17fd86902692e28c73ec6abff4078d18bb
Michal J. Sladek <michal.sladek@nav.no>
2024-12-05 13:18:02 +0100

    fix: bruker default på maxLifetime i connection pool

--- migreringer/src/test/java/no/nav/foreldrepenger/abakus/dbstoette/TestDatabaseInit.java
+++ migreringer/src/test/java/no/nav/foreldrepenger/abakus/dbstoette/TestDatabaseInit.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.abakus.dbstoette;
 
 import java.io.File;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.naming.NamingException;
@@ -78,9 +79,8 @@ public final class TestDatabaseInit {
         cfg.setJdbcUrl(jdbcUrl);
         cfg.setUsername(username);
         cfg.setPassword(password);
-        cfg.setConnectionTimeout(1500);
-        cfg.setValidationTimeout(120L * 1000L);
         cfg.setMaximumPoolSize(4);
+        cfg.setConnectionTimeout(TimeUnit.SECONDS.toMillis(2)); // feiler fast hvis db ikke kjører lokalt.
         cfg.setAutoCommit(false);
 
         var ds = new HikariDataSource(cfg);
