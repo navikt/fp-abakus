cbbb880a06e47a5d554b443f663a85bb24a03194
Michal J. Sladek <michal.sladek@nav.no>
2025-03-03 09:41:40 +0100

    Sonar

--- domenetjenester/iay/src/test/java/no/nav/foreldrepenger/abakus/domene/iay/InntektArbeidYtelseRepositoryTest.java
+++ domenetjenester/iay/src/test/java/no/nav/foreldrepenger/abakus/domene/iay/InntektArbeidYtelseRepositoryTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
