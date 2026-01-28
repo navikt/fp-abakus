822eee54f07adc7add3c700c11c5a600109b3d6e
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-01-28 10:00:09 +0100

    TFP-5437 fjerne roundtriptester fra kontrakten (#2754)

--- kontrakter/kontrakt/src/test/java/no/nav/foreldrepenger/kontrakter/iaygrunnlag/v1/iay/IAYValideringTest.java
+++ kontrakter/kontrakt/src/test/java/no/nav/foreldrepenger/kontrakter/iaygrunnlag/v1/iay/IAYValideringTest.java
@@ -5,24 +5,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.validation.Validation;
-
 import org.junit.jupiter.api.Test;
 
+import jakarta.validation.Validation;
 import no.nav.abakus.iaygrunnlag.Periode;
 import no.nav.abakus.iaygrunnlag.oppgittopptjening.v1.OppgittEgenNæringDto;
 
 class IAYValideringTest {
 
     @Test
-    void test_virksomhetnavn() throws Exception {
+    void test_virksomhetnavn() {
         var dto = new OppgittEgenNæringDto(new Periode(LocalDate.now(), LocalDate.now()));
         dto.setVirksomhetNavn("hello$£@£$1@£€6{[,æ'\"?`/$£æøåÆØÅ12340ø");
         validateResult(dto, true);
     }
 
     @Test
-    void test_feil_bruttoinntekt() throws Exception {
+    void test_feil_bruttoinntekt() {
         var dto = new OppgittEgenNæringDto(new Periode(LocalDate.now(), LocalDate.now()));
         dto.setBruttoInntekt(BigDecimal.valueOf(-1));
         validateResult(dto, false);
