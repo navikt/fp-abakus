e4a06f3ee68d3d56d14edf93993d0be2945924aa
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-01-29 09:00:31 +0100

    TFP-5437 bruk felles-mapper i Rest-laget (#2755)

--- src/main/java/no/nav/foreldrepenger/abakus/app/jackson/JacksonJsonConfig.java
+++ src/main/java/no/nav/foreldrepenger/abakus/app/jackson/JacksonJsonConfig.java
@@ -1,69 +1,21 @@
 package no.nav.foreldrepenger.abakus.app.jackson;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
-import no.nav.abakus.iaygrunnlag.v1.InntektArbeidYtelseGrunnlagDto;
-import no.nav.foreldrepenger.abakus.app.IndexClasses;
+import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 
 @Provider
 public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
 
-    private final ObjectMapper objectMapper;
-
-    /**
-     * Default instance for Jax-rs application. Genererer ikke navn som del av output for kodeverk.
-     */
-    public JacksonJsonConfig() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Jdk8Module());
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        registerSubTypesDynamically();
-    }
-
-    public static List<Class<?>> getKontraktLokasjoner() {
-        return List.of(JacksonJsonConfig.class, InntektArbeidYtelseGrunnlagDto.class);
-    }
-
-    /**
-     * Scan subtyper dynamisk fra utvalgte jar/war slik at superklasse slipper å deklarere @JsonSubtypes.
-     */
-    private static List<Class<?>> getJsonTypeNameClasses(URI classLocation) {
-        var indexClasses = IndexClasses.getIndexFor(classLocation);
-        return indexClasses.getClassesWithAnnotation(JsonTypeName.class);
-    }
-
-    private void registerSubTypesDynamically() {
-        // avled code location fra klassene
-        getKontraktLokasjoner().stream().map(c -> {
-            try {
-                return c.getProtectionDomain().getCodeSource().getLocation().toURI();
-            } catch (URISyntaxException e) {
-                throw new IllegalArgumentException("Ikke en URI for klasse: " + c, e);
-            }
-        }).distinct().forEach(uri -> getObjectMapper().registerSubtypes(getJsonTypeNameClasses(uri)));
-    }
-
-    public ObjectMapper getObjectMapper() {
-        return objectMapper;
-    }
+    private static final JsonMapper JM = DefaultJsonMapper.getJsonMapper();
+    
 
     @Override
     public ObjectMapper getContext(Class<?> type) {
-        return objectMapper;
+        return JM;
     }
 
 }
