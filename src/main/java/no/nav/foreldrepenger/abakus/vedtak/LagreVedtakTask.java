607c94b3ff37b986aecb478891a9054ca9ef07b7
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-03-20 12:45:44 +0100

    TFP-6900 publiser vedtak-ekstern etter lagring

--- src/main/java/no/nav/foreldrepenger/abakus/vedtak/LagreVedtakTask.java
+++ src/main/java/no/nav/foreldrepenger/abakus/vedtak/LagreVedtakTask.java
@@ -1,14 +1,17 @@
 package no.nav.foreldrepenger.abakus.vedtak;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import no.nav.abakus.vedtak.ytelse.Ytelse;
+import no.nav.abakus.vedtak.ytelse.Ytelser;
 import no.nav.abakus.vedtak.ytelse.v1.YtelseV1;
 import no.nav.foreldrepenger.abakus.vedtak.domene.VedtakYtelseBuilder;
 import no.nav.foreldrepenger.abakus.vedtak.domene.VedtakYtelseRepository;
@@ -16,8 +19,10 @@ import no.nav.foreldrepenger.abakus.vedtak.extract.v1.ExtractFromYtelseV1;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 
+@ApplicationScoped
 @ProsessTask(value = "vedtakEvent.lagre", prioritet = 2)
 public class LagreVedtakTask implements ProsessTaskHandler {
 
@@ -25,14 +30,16 @@ public class LagreVedtakTask implements ProsessTaskHandler {
 
     private VedtakYtelseRepository ytelseRepository;
     private ExtractFromYtelseV1 extractor;
+    private ProsessTaskTjeneste prosessTaskTjeneste;
 
     public LagreVedtakTask() {
     }
 
     @Inject
-    public LagreVedtakTask(VedtakYtelseRepository ytelseRepository, ExtractFromYtelseV1 extractor) {
+    public LagreVedtakTask(VedtakYtelseRepository ytelseRepository, ExtractFromYtelseV1 extractor, ProsessTaskTjeneste prosessTaskTjeneste) {
         this.ytelseRepository = ytelseRepository;
         this.extractor = extractor;
+        this.prosessTaskTjeneste = prosessTaskTjeneste;
     }
 
     @Override
@@ -58,6 +65,28 @@ public class LagreVedtakTask implements ProsessTaskHandler {
 
             ytelseRepository.lagre(builder);
 
+            var publiser = lagPubliserTaskData(mottattVedtak1, key);
+            prosessTaskTjeneste.lagre(publiser);
         }
+    }
+
+    private static ProsessTaskData lagPubliserTaskData(YtelseV1 mottattVedtak, String key) {
+        var publiser = ProsessTaskData.forProsessTask(PubliserVedtakTask.class);
+        publiser.setProperty(PubliserVedtakTask.KEY, Optional.ofNullable(mottattVedtak.getSaksnummer()).orElse(key));
+        if (mottattVedtak.getAktør().erAktørId()) {
+            publiser.setProperty(PubliserVedtakTask.AKTØRID, mottattVedtak.getAktør().getVerdi());
+        } else {
+            publiser.setProperty(PubliserVedtakTask.IDENT, mottattVedtak.getAktør().getVerdi());
+        }
+        publiser.setProperty(PubliserVedtakTask.TEMA, utledTema(mottattVedtak.getYtelse()));
+        return publiser;
+    }
+
+    private static String utledTema(Ytelser ytelse) {
+        return switch (ytelse) {
+            case ENGANGSTØNAD, FORELDREPENGER, SVANGERSKAPSPENGER -> "FOR";
+            case PLEIEPENGER_SYKT_BARN, PLEIEPENGER_NÆRSTÅENDE, OMSORGSPENGER, OPPLÆRINGSPENGER -> "OMS";
+            case FRISINN -> "FRI";
+        };
     }
 }
---
+++ src/main/java/no/nav/foreldrepenger/abakus/vedtak/PubliserVedtakTask.java
@@ -0,0 +1,65 @@
+package no.nav.foreldrepenger.abakus.vedtak;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import no.nav.foreldrepenger.abakus.aktor.AktørTjeneste;
+import no.nav.foreldrepenger.abakus.typer.AktørId;
+import no.nav.foreldrepenger.abakus.typer.PersonIdent;
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaSender;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.mapper.json.DefaultJsonMapper;
+
+@ApplicationScoped
+@ProsessTask(value = "vedtakEvent.publiser", prioritet = 2)
+public class PubliserVedtakTask implements ProsessTaskHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PubliserVedtakTask.class);
+
+    public static final String KEY = "publiser.key";
+    public static final String TEMA = "publiser.tema";
+    public static final String IDENT = "publiser.ident";
+    public static final String AKTØRID = "publiser.aktorid";
+
+    private AktørTjeneste aktørTjeneste;
+    private KafkaSender producer;
+
+    public PubliserVedtakTask() {
+    }
+
+    @Inject
+    public PubliserVedtakTask(AktørTjeneste aktørTjeneste, @KonfigVerdi(value = "kafka.eksternvedtak.topic") String topicName) {
+        this.aktørTjeneste = aktørTjeneste;
+        this.producer = new KafkaSender(topicName);
+    }
+
+    @Override
+    public void doTask(ProsessTaskData data) {
+        var key = Objects.requireNonNull(data.getPropertyValue(KEY));
+        var tema = Objects.requireNonNull(data.getPropertyValue(TEMA));
+        var identKey = data.getPropertyValue(IDENT);
+        var aktørId = data.getPropertyValue(AKTØRID);
+
+        var ident = Optional.ofNullable(aktørId).map(AktørId::new)
+            .flatMap(a -> aktørTjeneste.hentIdentForAktør(a))
+            .map(PersonIdent::getIdent)
+            .or(() -> Optional.ofNullable(identKey));
+        if (ident.isPresent()) {
+            LOG.info("Sender melding om vedtak med nøkkel {} på topic='{}'", key, producer.getTopicName());
+            var vedtak = new EksternVedtak(ident.get(), OffsetDateTime.now(), tema);
+            var recordMetadata = producer.send(key, DefaultJsonMapper.toJson(vedtak));
+            LOG.info("Sendte melding om vedtak til {}, partition {}, offset {}", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
+        }
+    }
+
+    public record EksternVedtak(String personidentifikator, OffsetDateTime tidspunkt, String tema) {}
+}
