bb27cd1736b0f0be912730daab8677f54c4665ed
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-06-17 21:34:37 +0200

    TFP-7017 bruker mer felles db-relatert kode, exit JNDI

--- src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevServer.java
+++ src/test/java/no/nav/foreldrepenger/abakus/jetty/JettyDevServer.java
@@ -1,17 +1,11 @@
 package no.nav.foreldrepenger.abakus.jetty;
 
-import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.FlywayException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.vedtak.server.localdev.LocalDevProperties;
 
 public class JettyDevServer extends JettyServer {
 
     private static final Environment ENV = Environment.current();
-    private static final Logger LOG = LoggerFactory.getLogger(JettyDevServer.class);
 
     private JettyDevServer(int serverPort) {
         super(serverPort);
@@ -29,24 +23,4 @@ public class JettyDevServer extends JettyServer {
         return new JettyDevServer(ENV.getProperty("server.port", Integer.class, 8015));
     }
 
-    @Override
-    void migrerDatabaser() {
-        try {
-            super.migrerDatabaser();
-        } catch (Exception _) {
-            LOG.info("Migreringer feilet, cleaner og prøver på nytt for lokal db.");
-            try (var migreringDs = DatasourceUtil.createDatasource(DatasourceRole.ADMIN, 2)) {
-                var flyway = Flyway.configure()
-                    .dataSource(migreringDs)
-                    .locations("classpath:/db/migration/")
-                    .baselineOnMigrate(true)
-                    .cleanDisabled(false)
-                    .load();
-                flyway.clean();
-            } catch (FlywayException fwe) {
-                throw new IllegalStateException("Migrering feiler.", fwe);
-            }
-            super.migrerDatabaser();
-        }
-    }
 }
